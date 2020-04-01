5c6c79c954a2cc43eaea1a2d9ae008642fd7d35c
wingkwong <wingkwong.code@gmail.com>
2020-04-01 20:36:43 +0800

    Add simplee-array-sum

---
+++ warmup/simple-array-sum/main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std; 
+
+int n, x, sum=0;
+
+int main()  
+{ 
+    ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+    cin >> n;
+    for(int i=0; i<n; ++i){
+        cin >> x;
+        sum += x;
+    }
+    cout << sum;
+    return 0; 
+} 
