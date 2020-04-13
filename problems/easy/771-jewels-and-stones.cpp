6401eb530912bee89c3934ae1aa4ba11015c240a
wingkwong <wingkwong.code@gmail.com>
2020-04-13 13:06:14 +0800

    Create 771-jewels-and-stones.cpp

---
+++ problems/easy/771-jewels-and-stones.cpp
@@ -0,0 +1,36 @@
+/*
+Jewels and Stones
+
+You're given strings J representing the types of stones that are jewels, and S representing the stones you have.  Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
+
+The letters in J are guaranteed distinct, and all characters in J and S are letters. Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+Example 1:
+
+Input: J = "aA", S = "aAAbbbb"
+Output: 3
+Example 2:
+
+Input: J = "z", S = "ZZ"
+Output: 0
+Note:
+
+S and J will consist of letters and have length at most 50.
+The characters in J are distinct.
+*/
+
+class Solution {
+public:
+    int numJewelsInStones(string J, string S) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        
+        unordered_map<char,int> m;
+        int ssz=S.size();
+        int jsz=J.size();
+        int ans=0;
+        for(int i=0;i<ssz;i++) m[S[i]]++;
+        for(int i=0;i<jsz;i++) if(m[J[i]]) ans+=m[J[i]];
+        return ans;
+    }
+};
