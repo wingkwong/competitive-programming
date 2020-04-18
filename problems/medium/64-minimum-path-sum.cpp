ec29c39d572cd5d046b4bf21cfa3d81ae1f35b37
wingkwong <wingkwong.code@gmail.com>
2020-04-18 21:40:33 +0800

    Add Solution2

--- problems/medium/64-minimum-path-sum.cpp
+++ problems/medium/64-minimum-path-sum.cpp
@@ -33,4 +33,28 @@ public:
     }
 };
 
+class Solution2 {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        if(grid.size()==0) return 0;
+        int m=grid.size();
+        int n=grid[0].size();
+        int dp[m][n];
+        memset(dp,0,sizeof(dp[0][0])*m*n);
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                dp[i][j]+=grid[i][j];
+                if(i>0&&j>0) {
+                    dp[i][j]+=min(dp[i-1][j], dp[i][j-1]);
+                } else if(i>0){
+                    dp[i][j]+=dp[i-1][j]; // take from the top
+                } else if(j>0){
+                    dp[i][j]+=dp[i][j-1]; // take from the left
+                }
+            }
+        }
+        return dp[m-1][n-1];
+    }
+};
+
 static const auto io_sync_off = []() {std::ios::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);return 0;}();
