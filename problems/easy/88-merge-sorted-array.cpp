6f06a299a5055cb858c1fd7cfcc3be01bf073814
wingkwong <wingkwong.code@gmail.com>
2020-04-19 18:46:35 +0800

    Update 88-merge-sorted-array.cpp

--- problems/easy/88-merge-sorted-array.cpp
+++ problems/easy/88-merge-sorted-array.cpp
@@ -19,18 +19,29 @@ Output: [1,2,2,3,5,6]
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int l=m+n;
-        while(n>0){
-            if(nums1[m-1]>nums2[n-1]){
-                nums1[--l] = nums1[--m];
-            } else {
-                nums1[--l] = nums2[--n];
+        int i=m-1,j=n-1,k=m+n-1;
+        // [1,2,3,0,0,0]
+        // 3
+        // [2,5,6]
+        // 3
+        while(i>=0&&j>=0){
+            if(nums1[i]>nums2[j]){
+                nums1[k]=nums1[i];
+                k--; i--;
+            } else{
+                nums1[k]=nums2[j];
+                k--; j--;
             }
-            // 1 2 3 0 0 6 
-            // 1 2 3 0 5 6 
-            // 1 2 3 3 5 6 
-            // 1 2 2 3 5 6 
         }
+        // [0]
+        // 0
+        // [1]
+        // 1
+        while(j>=0){
+            nums1[k]=nums2[j];
+            k--; j--;
+        }
+        
     }
 };
 
