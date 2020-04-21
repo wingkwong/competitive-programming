170fb95e94a80c25069f31b9010d94bb5e353185
wingkwong <wingkwong.code@gmail.com>
2020-04-21 19:39:37 +0800

    Create day21.cpp

---
+++ 30days-challenge/week3/day21.cpp
@@ -0,0 +1,84 @@
+/*
+Leftmost Column with at Least a One
+
+(This problem is an interactive problem.)
+
+A binary matrix means that all elements are 0 or 1. For each individual row of the matrix, this row is sorted in non-decreasing order.
+
+Given a row-sorted binary matrix binaryMatrix, return leftmost column index(0-indexed) with at least a 1 in it. If such index doesn't exist, return -1.
+
+You can't access the Binary Matrix directly.  You may only access the matrix using a BinaryMatrix interface:
+
+BinaryMatrix.get(x, y) returns the element of the matrix at index (x, y) (0-indexed).
+BinaryMatrix.dimensions() returns a list of 2 elements [n, m], which means the matrix is n * m.
+Submissions making more than 1000 calls to BinaryMatrix.get will be judged Wrong Answer.  Also, any solutions that attempt to circumvent the judge will result in disqualification.
+
+For custom testing purposes you're given the binary matrix mat as input in the following four examples. You will not have access the binary matrix directly.
+
+ 
+
+Example 1:
+
+Input: mat = [[0,0],[1,1]]
+Output: 0
+Example 2:
+
+Input: mat = [[0,0],[0,1]]
+Output: 1
+
+Example 3:
+
+Input: mat = [[0,0],[0,0]]
+Output: -1
+
+Example 4:
+
+Input: mat = [[0,0,0,1],[0,0,1,1],[0,1,1,1]]
+Output: 1
+ 
+
+Constraints:
+
+1 <= mat.length, mat[i].length <= 100
+mat[i][j] is either 0 or 1.
+mat[i] is sorted in a non-decreasing way.
+*/
+
+/*
+Approach:
+
+1. (Binary Search) For each row do a binary search to find the leftmost one on that row and update the answer.
+2. (Optimal Approach) Imagine there is a pointer p(x, y) starting from top right corner. p can only move left or down. 
+    If the value at p is 0, move down. If the value at p is 1, move left. 
+*/
+
+
+/**
+ * // This is the BinaryMatrix's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * class BinaryMatrix {
+ *   public:
+ *     int get(int x, int y);
+ *     vector<int> dimensions();
+ * };
+ */
+
+class Solution {
+public:
+    int leftMostColumnWithOne(BinaryMatrix &binaryMatrix) {
+        bool found=false;
+        vector<int> d = binaryMatrix.dimensions();
+        int i=0,j=d[1]-1,m=d[0]-1,ans=-1;
+        while(i<=m&&j>=0){
+            if(binaryMatrix.get(i,j)){
+                ans=j;
+                j--; // move left
+            } else {
+                i++; // move down
+            }
+        }
+        return ans;
+    }
+};
+
+static const auto io_sync_off = []() {std::ios::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);return 0;}();
