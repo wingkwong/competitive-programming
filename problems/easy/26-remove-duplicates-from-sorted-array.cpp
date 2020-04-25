4ea0d6e48bf4fcde14628474b0e516b1c9204c32
wingkwong <wingkwong.code@gmail.com>
2020-04-25 18:10:36 +0800

    Update 26-remove-duplicates-from-sorted-array.cpp

--- problems/easy/26-remove-duplicates-from-sorted-array.cpp
+++ problems/easy/26-remove-duplicates-from-sorted-array.cpp
@@ -45,9 +45,13 @@ public:
         int i=0;
         for(int j=1;j<nums.size();j++){
             if(nums[j]!=nums[i]){
+                // We need to modify the array in-place and the size of the final array would potentially be smaller than the size of the input array. 
+                // So, we ought to use a two-pointer approach here. 
+                // One, that would keep track of the current element in the original array and another one for just the unique elements.
                 i++;
                 nums[i]=nums[j];
             }
+            // Essentially, once an element is encountered, you simply need to bypass its duplicates and move on to the next unique element.
         }
         return i+1;
     }
