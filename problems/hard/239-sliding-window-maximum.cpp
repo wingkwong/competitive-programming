2aa2eff56de3e02c0a6434ec1fdc754f7d2fa581
wingkwong <wingkwong.code@gmail.com>
2020-04-27 23:14:03 +0800

    Create 239-sliding-window-maximum.cpp

---
+++ problems/hard/239-sliding-window-maximum.cpp
@@ -0,0 +1,52 @@
+/*
+Sliding Window Maximum
+
+Given an array nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position. Return the max sliding window.
+
+Follow up:
+Could you solve it in linear time?
+
+Example:
+
+Input: nums = [1,3,-1,-3,5,3,6,7], and k = 3
+Output: [3,3,5,5,6,7] 
+Explanation: 
+
+Window position                Max
+---------------               -----
+[1  3  -1] -3  5  3  6  7       3
+ 1 [3  -1  -3] 5  3  6  7       3
+ 1  3 [-1  -3  5] 3  6  7       5
+ 1  3  -1 [-3  5  3] 6  7       5
+ 1  3  -1  -3 [5  3  6] 7       6
+ 1  3  -1  -3  5 [3  6  7]      7
+ 
+
+Constraints:
+
+1 <= nums.length <= 10^5
+-10^4 <= nums[i] <= 10^4
+1 <= k <= nums.length
+*/
+
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int> q;
+        vector<int> ans;
+        for(int i=0;i<nums.size();i++){
+            // remove the elements which are out of this window
+            while(!q.empty() && q.front()<=i-k) q.pop_front();
+            // remove all elements smaller than the currently 
+            // being added element (remove useless elements)
+            while(!q.empty() && nums[i] >= nums[q.back()]) q.pop_back();
+            // add the current element at the end of q
+            q.push_back(i);
+            // add the front before sliding a window
+            if(i>=k-1) ans.push_back(nums[q.front()]);
+        }
+        return ans;
+    }
+};
+
+static const auto io_sync_off = []() {std::ios::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);return 0;}();
