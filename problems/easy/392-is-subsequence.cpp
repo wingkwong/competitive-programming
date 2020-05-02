3a8df2cd0c0335a47ff54f741a79961ee9b9940a
wingkwong <wingkwong.code@gmail.com>
2020-05-02 12:40:10 +0800

    Create 392-is-subsequence.cpp

---
+++ problems/easy/392-is-subsequence.cpp
@@ -0,0 +1,43 @@
+/*
+Is Subsequence
+
+Given a string s and a string t, check if s is subsequence of t.
+
+You may assume that there is only lower case English letters in both s and t. t is potentially a very long (length ~= 500,000) string, and s is a short string (<=100).
+
+A subsequence of a string is a new string which is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (ie, "ace" is a subsequence of "abcde" while "aec" is not).
+
+Example 1:
+s = "abc", t = "ahbgdc"
+
+Return true.
+
+Example 2:
+s = "axc", t = "ahbgdc"
+
+Return false.
+
+Follow up:
+If there are lots of incoming S, say S1, S2, ... , Sk where k >= 1B, and you want to check one by one to see if T has its subsequence. In this scenario, how would you change your code?
+
+Credits:
+Special thanks to @pbrother for adding this problem and creating all test cases.
+*/
+
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int ssz=(int)s.size();
+        int tsz=(int)t.size();
+        int i=0,j=0;
+        // two pointer approach
+        for(;i<ssz&&j<tsz;j++){
+            // if matched, move i to the next char in s
+            if(s[i]==t[j]) i++;
+        }
+        // if i reaches the end, it means it is a subseqeunce
+        return (i==ssz);
+    }
+};
+
+static const auto io_sync_off = []() {std::ios::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);return 0;}();
