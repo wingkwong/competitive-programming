67435aa62f883aeed17629e62e5bc25e9b5aa08b
wingkwong <wingkwong.code@gmail.com>
2020-05-10 16:22:38 +0800

    Add tempate

---
+++ template/E.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std; 
+
+#define FAST_INP  ios_base::sync_with_stdio(false);cin.tie(NULL)
+int main()  
+{ 
+    FAST_INP;
+    int t; 
+    cin >> t;
+    while(t--){
+        
+    }
+    return 0;
+} 
