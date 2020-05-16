354c34224a7744b6e05014ad651844942428d395
wingkwong <wingkwong.code@gmail.com>
2020-05-16 19:13:45 +0800

    Add Contest Template

---
+++ template/B.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std; 
+#define FAST_INP  ios_base::sync_with_stdio(false);cin.tie(NULL)
+
+int main()  
+{ 
+    FAST_INP;
+    int n,t;
+    cin >> t;
+    while(t--){
+        cin >> n;
+        
+    }
+    return 0;
+} 
