8195d36dbbc072a7387a3f34bbe8139ba28d06ae
wingkwong <wingkwong.code@gmail.com>
2020-06-09 17:18:38 +0800

    Update 392-is-subsequence.cpp

--- problems/easy/392-is-subsequence.cpp
+++ problems/easy/392-is-subsequence.cpp
@@ -40,4 +40,24 @@ public:
     }
 };
 
+class Solution2 {
+public:
+    bool isSubsequence(string s, string t) {
+        // DP approach - similar to 1143. Longest Common Subsequence
+        int m=(int)s.size(), n=(int)t.size();
+        if(m==0) return true;
+        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(s[i-1]==t[j-1]) {
+                    dp[i][j]=dp[i-1][j-1]+1;
+                } else {
+                    dp[i][j]=max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return dp[m][n]==m;
+    }
+};
+
 static const auto io_sync_off = []() {std::ios::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);return 0;}();
