3ded9468a37414a139f8870e11303979b8787d38
wingkwong <wingkwong.code@gmail.com>
2020-07-09 16:51:35 +0800

    Update 1072-two-knights.cpp

--- introductory-problems/1072-two-knights.cpp
+++ introductory-problems/1072-two-knights.cpp
@@ -43,13 +43,13 @@ int main()
         long long k2 = k*k;
         // the second knight can be placed in k2-1 ways
         // the number of ways to place two knights is k2*(k2-1)
-        // two knights cannot be placed in the same cells, de-duplicate by diviving by 2, i.e. k2*(k2-1)/2
+        // since the order doesn't matter, so divide k2*(k2-1) by 2
         // a knight can attack in a form of 2x3 and 3x2 chessboard, 
+        // then we need to find out how many 2x3 and 3x2 can be filled in nxn
         // for 2x3: (n-2+1)(n-3+1) = (n-1)(n-2)
-        // for 3x2: (n-3+1)(n-2+1) = (n-2)(n-1)
+        // for 3x2: (n-3+1)(n-2+1) = (n-2)(n-1) 
         // hence 2x3 + 3x2: (n-1)(n-2) + (n-2)(n-1) -> 2(n-1)(n-2)
-        // the two knights can be swapped, so multiply by 2
-        // combine it together, that would be 4(k-1)(k-2)
+        // the two knights can be swapped, so multiply it by 2 and we got 4(k-1)(k-2)
         cout << (k2*(k2-1)/2 - 4*(k-1)*(k-2)) << "\n";
     }
     return 0;
