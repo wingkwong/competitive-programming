a3d00210d74632bddda7be5a6e97ee503fb84fc6
wingkwong <wingkwong.code@gmail.com>
2020-07-19 18:24:04 +0800

    Create 1325A.cpp

---
+++ practices/800/1325A.cpp
@@ -0,0 +1,20 @@
+/*
+Codeforces Round #628 (Div. 2) - A. EhAb AnD gCd
+https://codeforces.com/problemset/problem/1325/A
+*/
+
+#include <bits/stdc++.h>
+using namespace std; 
+#define FAST_INP  ios_base::sync_with_stdio(false);cin.tie(NULL)
+
+int main()  
+{ 
+    FAST_INP;
+    int t,x;
+    cin >> t;
+    while(t--){
+    	cin >> x;
+    	cout << 1 << " " << x-1 << endl;
+	}
+    return 0;
+} 
