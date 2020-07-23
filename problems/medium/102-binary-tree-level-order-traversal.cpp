00ceb067bfffd5a88101f10d5203d98e4bd13876
wingkwong <wingkwong.code@gmail.com>
2020-07-23 19:27:52 +0800

    Update 102-binary-tree-level-order-traversal.cpp

--- problems/medium/102-binary-tree-level-order-traversal.cpp
+++ problems/medium/102-binary-tree-level-order-traversal.cpp
@@ -30,28 +30,38 @@ return its level order traversal as:
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
         vector<vector<int>> ans;
+        // if null, return {}
         if(!root) return ans;
         queue<TreeNode*> q;
         q.push(root);
         while(!q.empty()){
-            int sz = q.size();
-            vector<int> t;
-            for(int i=0;i<sz;i++){
-                // current node
-                TreeNode* n = q.front();
-                // remove current node
+            // travese each level
+            int n = q.size();
+            vector<int> cur(n);
+            for(int i=0;i<n;i++){
+                // traverse each element at the current level
+                TreeNode* tmp = q.front();
                 q.pop();
-                // node root
-                t.push_back(n->val);
-                    
-                if(n->left) q.push(n->left);
-                if(n->right) q.push(n->right);
+                cur[i] = tmp->val;
+                if(tmp->left) q.push(tmp->left);
+                if(tmp->right) q.push(tmp->right);
             }
-            ans.push_back(t);
+            ans.emplace_back(cur);
         }
         return ans;
     }
