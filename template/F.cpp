ae4bd393794d02513b1f5f495923da7515cdd73a
wingkwong <wingkwong.code@gmail.com>
2020-07-30 21:19:47 +0800

    Update Template

--- template/F.cpp
+++ template/F.cpp
@@ -1,11 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std; 
 #define FAST_INP  ios_base::sync_with_stdio(false);cin.tie(NULL)
+typedef long long ll; 
+const ll MOD = 1000000007;
 
 int main()  
 { 
     FAST_INP;
-    int n,t;
+    int t,n;
     cin >> t;
     while(t--){
         cin >> n;
