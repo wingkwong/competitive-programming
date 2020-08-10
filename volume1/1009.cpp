63614c61d630c5d9908251219448aa674b2bb9b0
wingkwong <wingkwong.code@gmail.com>
2020-08-10 20:28:18 +0800

    Create 1009.cpp

---
+++ volume1/1009.cpp
@@ -0,0 +1,19 @@
+/*
+Volume 1 - 1009. K-based Numbers
+https://acm.timus.ru/problem.aspx?space=1&num=1009
+*/
+
+#include <bits/stdc++.h>
+using namespace std; 
+#define FAST_INP  ios_base::sync_with_stdio(false);cin.tie(NULL)
+typedef long long ll; 
+const ll MOD = 1000000007;
+
+
+int main()  
+{ 
+    FAST_INP;
+    ll n, k;
+    cin >> n >> k;
+    return 0;
+} 
