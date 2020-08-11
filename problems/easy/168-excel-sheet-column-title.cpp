7315da8d833d36fc06e0af0d3807fe935324a170
wingkwong <wingkwong.code@gmail.com>
2020-08-11 11:13:22 +0800

    Create 168-excel-sheet-column-title.cpp

---
+++ problems/easy/168-excel-sheet-column-title.cpp
@@ -0,0 +1,36 @@
+/*
+Excel Sheet Column Title    
+
+Given a positive integer, return its corresponding column title as appear in an Excel sheet.
+
+For example:
+
+    1 -> A
+    2 -> B
+    3 -> C
+    ...
+    26 -> Z
+    27 -> AA
+    28 -> AB 
+    ...
+Example 1:
+
+Input: 1
+Output: "A"
+Example 2:
+
+Input: 28
+Output: "AB"
+Example 3:
+
+Input: 701
+Output: "ZY"
+*/
+
+class Solution {
+public:
+    string convertToTitle(int n) {
+        if(--n<26) return string(1,n+65);
+        return convertToTitle(n/26)+string(1,n%26+65);
+    }
+};
