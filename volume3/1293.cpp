953752f371385469e8478f13f79b2f6f88f76af2
wingkwong <wingkwong.code@gmail.com>
2020-08-11 19:29:02 +0800

    Create 1293.cpp

---
+++ volume3/1293.cpp
@@ -0,0 +1,19 @@
+/*
+Volume 3 - 1293. Eniya
+https://acm.timus.ru/problem.aspx?space=1&num=1293
+*/
+
+#include <bits/stdc++.h>
+using namespace std; 
+#define FAST_INP  ios_base::sync_with_stdio(false);cin.tie(NULL)
+typedef long long ll; 
+const ll MOD = 1000000007;
+
+int main()  
+{ 
+    FAST_INP;
+    ll n,a,b;
+    cin >> n >> a >> b;
+    cout << n*a*b*2 << endl;
+    return 0;
+} 
