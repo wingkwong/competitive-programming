b81766d5f9497659c69b9bc5c207fbae80870d09
wingkwong <wingkwong.code@gmail.com>
2020-08-12 21:35:52 +0800

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,11 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std; 
-
 #define FAST_INP  ios_base::sync_with_stdio(false);cin.tie(NULL)
+typedef long long ll; 
+const ll MOD = 1000000007;
+
 int main()  
 { 
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r", stdin);
+    freopen("output.txt","w", stdout);
+    #endif
     FAST_INP;
-
-    
+    int t,n;
+    cin >> t;
+    while(t--){
+        cin >> n;
+        
+    }
     return 0;
 } 
