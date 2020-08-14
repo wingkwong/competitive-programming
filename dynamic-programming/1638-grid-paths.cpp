76338901c6c13c1980fd2d3952d7540ba54184f2
wingkwong <wingkwong.code@gmail.com>
2020-08-14 20:56:22 +0800

    Create 1638-grid-paths.cpp

---
+++ dynamic-programming/1638-grid-paths.cpp
@@ -0,0 +1,117 @@
+/*
+Grid Paths
+https://cses.fi/problemset/task/1638
+
+Consider an n×n grid whose squares may have traps. It is not allowed to move to a square with a trap.
+
+Your task is to calculate the number of paths from the upper-left square to the lower-right square where you only can move right or down.
+
+Input
+
+The first input line has an integer n: the size of the grid.
+
+After this, there are n lines that describe the grid. Each line has n characters: . denotes an empty cell, and * denotes a trap.
+
+Output
+
+Print the number of paths modulo 109+7.
+
+Constraints
+1≤n≤1000
+Example
+
+Input:
+4
+....
+.*..
+...*
+*...
+
+Output:
+3
+*/
+
+#include <bits/stdc++.h>
+using namespace std; 
+
+typedef long long ll; 
+typedef pair<int, int> pii; 
+typedef pair<ll, ll> pll; 
+typedef pair<string, string> pss; 
+typedef vector<int> vi; 
+typedef vector<vi> vvi; 
+typedef vector<pii> vii; 
+typedef vector<pll> vll; 
+typedef vector<ll> vl; 
+typedef vector<vl> vvl; 
+
+double EPS=1e-9; 
+int INF=1000000005; 
+ll INFF=1000000000000000005ll; 
+double PI=acos(-1); 
+int dirx[8]={ -1, 0, 0, 1, -1, -1, 1, 1 }; 
+int diry[8]={ 0, 1, -1, 0, -1, 1, -1, 1 }; 
+const ll MOD = 1000000007;
+
+#define DEBUG fprintf(stderr, "====TESTING====\n") 
+#define VALUE(x) cerr << "The value of " << #x << " is " << x << endl 
+#define OUT(x) cout << x << endl 
+#define debug(...) fprintf(stderr, __VA_ARGS__) 
+#define READ(b) for(auto &(a):b) cin >> a;
+#define FOR(a, b, c) for (int(a)=(b); (a) < (c); ++(a)) 
+#define FORN(a, b, c) for (int(a)=(b); (a) <= (c); ++(a)) 
+#define FORD(a, b, c) for (int(a)=(b); (a) >= (c); --(a)) 
+#define FORSQ(a, b, c) for (int(a)=(b); (a) * (a) <= (c); ++(a)) 
+#define FORC(a, b, c) for (char(a)=(b); (a) <= (c); ++(a)) 
+#define EACH(a, b) for (auto&(a) : (b)) 
+#define REP(i, n) FOR(i, 0, n) 
+#define REPN(i, n) FORN(i, 1, n) 
+#define MAX(a, b) a=max(a, b) 
+#define MIN(a, b) a=min(a, b) 
+#define SQR(x) ((ll)(x) * (x)) 
+#define RESET(a, b) memset(a, b, sizeof(a)) 
+#define fi first 
+#define se second 
+#define mp make_pair 
+#define pb push_back 
+#define ALL(v) v.begin(), v.end() 
+#define ALLA(arr, sz) arr, arr + sz 
+#define SIZE(v) (int)v.size() 
+#define SORT(v) sort(ALL(v)) 
+#define REVERSE(v) reverse(ALL(v)) 
+#define SORTA(arr, sz) sort(ALLA(arr, sz)) 
+#define REVERSEA(arr, sz) reverse(ALLA(arr, sz)) 
+#define PERMUTE next_permutation 
+#define TC(t) while (t--) 
+#define FAST_INP  ios_base::sync_with_stdio(false);cin.tie(NULL)
+
+
+int main()  
+{ 
+    FAST_INP;
+    // #ifndef ONLINE_JUDGE
+    // freopen("input.txt","r", stdin);
+    // freopen("output.txt","w", stdout);
+    // #endif
+    int n;
+    cin >> n;
+    int dp[n][n];
+    memset(dp,0,sizeof(dp));
+    dp[0][0]=1;
+    vector<string> g(n);
+    REP(i,n) {
+        cin >> g[i];
+        REP(j,n){
+            if(g[i][j]=='.'){
+                // get from the left
+                if(i) dp[i][j] = (dp[i][j]+dp[i-1][j])%MOD;
+                // get from the top
+                if(j) dp[i][j] = (dp[i][j]+dp[i][j-1])%MOD;
+            } else {
+                dp[i][j] = 0;
+            }
+        }
+    }
+    OUT(dp[n-1][n-1]);
+    return 0; 
+} 
