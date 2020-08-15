7ec04bd6a05aebde91a0e5b4845b6dae95a42a41
wingkwong <wingkwong.code@gmail.com>
2020-08-15 11:44:06 +0800

    Create 1744-rectangle-cutting.cpp

---
+++ dynamic-programming/1744-rectangle-cutting.cpp
@@ -0,0 +1,105 @@
+/*
+Rectangle Cutting
+https://cses.fi/problemset/task/1744
+
+Given an a×b rectangle, your task is to cut it into squares. On each move you can select a rectangle and cut it into two rectangles in such a way that all side lengths remain integers. What is the minimum possible number of moves?
+
+Input
+
+The only input line has two integers a and b.
+
+Output
+
+Print one integer: the minimum number of moves.
+
+Constraints
+1≤a,b≤500
+Example
+
+Input:
+3 5
+
+Output:
+3
+*/
+
+#include <bits/stdc++.h>
+using namespace std; 
+
+typedef long long ll; 
+typedef pair<int, int> pii; 
+typedef pair<ll, ll> pll; 
+typedef pair<string, string> pss; 
+typedef vector<int> vi; 
+typedef vector<vi> vvi; 
+typedef vector<pii> vii; 
+typedef vector<pll> vll; 
+typedef vector<ll> vl; 
+typedef vector<vl> vvl; 
+
+double EPS=1e-9; 
+int INF=1000000005; 
+ll INFF=1000000000000000005ll; 
+double PI=acos(-1); 
+int dirx[8]={ -1, 0, 0, 1, -1, -1, 1, 1 }; 
+int diry[8]={ 0, 1, -1, 0, -1, 1, -1, 1 }; 
+const ll MOD = 1000000007;
+
+#define DEBUG fprintf(stderr, "====TESTING====\n") 
+#define VALUE(x) cerr << "The value of " << #x << " is " << x << endl 
+#define OUT(x) cout << x << endl 
+#define debug(...) fprintf(stderr, __VA_ARGS__) 
+#define READ(b) for(auto &(a):b) cin >> a;
+#define FOR(a, b, c) for (int(a)=(b); (a) < (c); ++(a)) 
+#define FORN(a, b, c) for (int(a)=(b); (a) <= (c); ++(a)) 
+#define FORD(a, b, c) for (int(a)=(b); (a) >= (c); --(a)) 
+#define FORSQ(a, b, c) for (int(a)=(b); (a) * (a) <= (c); ++(a)) 
+#define FORC(a, b, c) for (char(a)=(b); (a) <= (c); ++(a)) 
+#define EACH(a, b) for (auto&(a) : (b)) 
+#define REP(i, n) FOR(i, 0, n) 
+#define REPN(i, n) FORN(i, 1, n) 
+#define MAX(a, b) a=max(a, b) 
+#define MIN(a, b) a=min(a, b) 
+#define SQR(x) ((ll)(x) * (x)) 
+#define RESET(a, b) memset(a, b, sizeof(a)) 
+#define fi first 
+#define se second 
+#define mp make_pair 
+#define pb push_back 
+#define ALL(v) v.begin(), v.end() 
+#define ALLA(arr, sz) arr, arr + sz 
+#define SIZE(v) (int)v.size() 
+#define SORT(v) sort(ALL(v)) 
+#define REVERSE(v) reverse(ALL(v)) 
+#define SORTA(arr, sz) sort(ALLA(arr, sz)) 
+#define REVERSEA(arr, sz) reverse(ALLA(arr, sz)) 
+#define PERMUTE next_permutation 
+#define TC(t) while (t--) 
+#define FAST_INP  ios_base::sync_with_stdio(false);cin.tie(NULL)
+
+
+int main()  
+{ 
+    FAST_INP;
+    // #ifndef ONLINE_JUDGE
+    // freopen("input.txt","r", stdin);
+    // freopen("output.txt","w", stdout);
+    // #endif
+    int a,b;
+    cin >> a >> b;
+    int dp[a+1][b+1];
+    FORN(i,0,a){
+        FORN(j,0,b){
+            if(i==j) dp[i][j]=0; //square already
+            else{
+                dp[i][j]=INF;
+                // horizontal cuts
+                FOR(k,1,i) MIN(dp[i][j],dp[k][j]+dp[i-k][j]+1);
+                // vectical cuts
+                FOR(k,1,j) MIN(dp[i][j],dp[i][k]+dp[i][j-k]+1);
+            }
+        }
+    }
+    OUT(dp[a][b]);
+    return 0; 
+} 
