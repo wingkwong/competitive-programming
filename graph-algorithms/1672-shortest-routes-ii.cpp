852237ecaca29ea21085ed8a4185d516faf38750
wingkwong <wingkwong.code@gmail.com>
2020-08-19 21:38:06 +0800

    Create 1672-shortest-routes-ii.cpp

---
+++ graph-algorithms/1672-shortest-routes-ii.cpp
@@ -0,0 +1,93 @@
+/*
+Shortest Routes II
+https://cses.fi/problemset/task/1672
+*/
+
+#include <bits/stdc++.h>
+using namespace std; 
+
+typedef long long ll; 
+typedef pair<int, int> pii; 
+typedef pair<ll, ll> pll; 
+typedef pair<string, string> pss; 
+typedef vector<int> vi; 
+typedef vector<vi> vvi; 
+typedef vector<pii> vii; 
+typedef vector<pll> vll; 
+typedef vector<ll> vl; 
+typedef vector<vl> vvl; 
+
+double EPS=1e-9; 
+int INF=1000000005; 
+ll INFF=1000000000000000005ll; 
+double PI=acos(-1); 
+int dirx[8]={ -1, 0, 0, 1, -1, -1, 1, 1 }; 
+int diry[8]={ 0, 1, -1, 0, -1, 1, -1, 1 }; 
+const ll MOD = 1000000007;
+
+#define DEBUG fprintf(stderr, "====TESTING====\n") 
+#define VALUE(x) cerr << "The value of " << #x << " is " << x << endl 
+#define OUT(x) cout << x << endl 
+#define debug(...) fprintf(stderr, __VA_ARGS__) 
+#define READ(b) for(auto &(a):b) cin >> a;
+#define FOR(a, b, c) for (int(a)=(b); (a) < (c); ++(a)) 
+#define FORN(a, b, c) for (int(a)=(b); (a) <= (c); ++(a)) 
+#define FORD(a, b, c) for (int(a)=(b); (a) >= (c); --(a)) 
+#define FORSQ(a, b, c) for (int(a)=(b); (a) * (a) <= (c); ++(a)) 
+#define FORC(a, b, c) for (char(a)=(b); (a) <= (c); ++(a)) 
+#define EACH(a, b) for (auto&(a) : (b)) 
+#define REP(i, n) FOR(i, 0, n) 
+#define REPN(i, n) FORN(i, 1, n) 
+#define MAX(a, b) a=max(a, b) 
+#define MIN(a, b) a=min(a, b) 
+#define SQR(x) ((ll)(x) * (x)) 
+#define RESET(a, b) memset(a, b, sizeof(a)) 
+#define fi first 
+#define se second 
+#define mp make_pair 
+#define pb push_back 
+#define ALL(v) v.begin(), v.end() 
+#define ALLA(arr, sz) arr, arr + sz 
+#define SIZE(v) (int)v.size() 
+#define SORT(v) sort(ALL(v)) 
+#define REVERSE(v) reverse(ALL(v)) 
+#define SORTA(arr, sz) sort(ALLA(arr, sz)) 
+#define REVERSEA(arr, sz) reverse(ALLA(arr, sz)) 
+#define PERMUTE next_permutation 
+#define TC(t) while (t--) 
+#define FAST_INP  ios_base::sync_with_stdio(false);cin.tie(NULL)
+
+int main()  
+{ 
+    FAST_INP;
+    // #ifndef ONLINE_JUDGE
+    // freopen("input.txt","r", stdin);
+    // freopen("output.txt","w", stdout);
+    // #endif
+    ll n,m,q,a,b,c;
+    cin >> n >> m >> q;
+    const int mxN=505;
+    // Floyd Warshall Algorithm 
+    vector<vl> adj(n,vl(n,INFF));
+    REP(i,m){
+    	cin >> a >> b >> c;
+    	--a,--b;
+    	MIN(adj[a][b],c);
+		MIN(adj[b][a],c);
+	}
+	REP(i,n) adj[i][i]=0;
+	REP(k,n){
+		REP(i,n){
+			REP(j,n){
+//				adj[i][j]=adj[i][j]||(adj[i][k]&&adj[k][j]);
+				MIN(adj[i][j],adj[i][k]+adj[k][j]);
+			}
+		}
+	}
+	REP(i,q){
+		cin >> a >> b;
+		--a, --b;
+		OUT((adj[a][b]>=INFF?-1:adj[a][b]));
+	}
+    return 0; 
+} 
