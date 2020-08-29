cba09f65e265976de5e50ac62f19bb620d0d857b
wingkwong <wingkwong.code@gmail.com>
2020-08-29 19:43:48 +0800

    Create H-Index.cpp

---
+++ binarysearch.io/medium/H-Index.cpp
@@ -0,0 +1,15 @@
+#include "solution.hpp"
+using namespace std;
+
+
+class Solution {
+    public:
+    int solve(vector<int>& citations) {
+        sort(citations.begin(), citations.end());
+        int n = citations.size();
+        for(int i=0;i<n;i++){
+            if(citations[i]>=n-i) return n-i;
+        }
+        return 0;
+    }
+};
