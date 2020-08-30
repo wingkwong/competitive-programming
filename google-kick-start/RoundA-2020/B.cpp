75c68aa5774e729fe26e0a064d394339287ea007
wingkwong <wingkwong.code@gmail.com>
2020-08-30 16:01:18 +0800

    Create B.cpp

---
+++ google-kick-start/RoundA-2020/B.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+using namespace std; 
+
+typedef long long ll; 
+typedef pair<int, int> pii; 
+typedef pair<ll, ll> pll; 
+typedef pair<string, string> pss; 
+typedef vector<int> vi; 
+typedef vector<vi> vvi; 
+typedef vector<pii> vii; 
+typedef vector<ll> vl; 
+typedef vector<vl> vvl; 
+
+double EPS=1e-9; 
+int INF=1000000005; 
+long long INFF=1000000000000000005ll; 
+double PI=acos(-1); 
+int dirx[8]={ -1, 0, 0, 1, -1, -1, 1, 1 }; 
+int diry[8]={ 0, 1, -1, 0, -1, 1, -1, 1 }; 
+const ll MOD = 1000000007;
+
+#define DEBUG fprintf(stderr, "====TESTING====\n") 
+#define VALUE(x) cerr << "The value of " << #x << " is " << x << endl 
+#define OUT(x) cout << x << endl 
+#define debug(...) fprintf(stderr, __VA_ARGS__) 
+#define READ(x) for(auto &(z):x) cin >> z;
+#define FOR(a, b, c) for (int(a)=(b); (a) < (c); ++(a)) 
+#define FORN(a, b, c) for (int(a)=(b); (a) <= (c); ++(a)) 
+#define FORD(a, b, c) for (int(a)=(b); (a) >= (c); --(a)) 
+#define FORSQ(a, b, c) for (int(a)=(b); (a) * (a) <= (c); ++(a)) 
+#define FORC(a, b, c) for (char(a)=(b); (a) <= (c); ++(a)) 
+#define EACH(a, b) for (auto&(a) : (b)) 
+#define REP(i, n) FOR(i, 0, n) 
+#define REPN(i, n) FORN(i, 1, n) 
+#define MAX(a, b) a=max(a, b) 
+#define MIN(a, b) a=min(a, b) 
+#define SQR(x) ((ll)(x) * (x)) 
+#define RESET(a, b) memset(a, b, sizeof(a)) 
+#define fi first 
+#define se second 
+#define mp make_pair 
+#define pb push_back 
+#define ALL(v) v.begin(), v.end() 
+#define ALLA(arr, sz) arr, arr + sz 
+#define SIZE(v) (int)v.size() 
+#define SORT(v) sort(ALL(v)) 
+#define REVERSE(v) reverse(ALL(v)) 
+#define SORTA(arr, sz) sort(ALLA(arr, sz)) 
+#define REVERSEA(arr, sz) reverse(ALLA(arr, sz)) 
+#define PERMUTE next_permutation 
+#define TC(t) while (t--) 
+#define FAST_INP  ios_base::sync_with_stdio(false);cin.tie(NULL)
+
+int case_no = 1;
+
+void solve() {
+	int n, k, p, b;
+	cin >> n >> k >> p;
+	vvi sum(n+1,vi((n*k)+1,0)), dp(n+1,vi((n*k)+1,0));
+	FORN(i,1,n){
+		FORN(j,1,k){
+			cin >> b;
+			sum[i][j]=sum[i][j-1]+b;
+		}
+	}
+	FORN(i,1,k) dp[1][i]=sum[1][i];
+	FORN(i,2,n) FORN(j,1,p) FORN(x,0,min(j,k)) MAX(dp[i][j],dp[i-1][j-x]+sum[i][x]);
+	cout << "Case #" << case_no++ << ": " << dp[n][p] << "\n";
+}
+
+int main()  
+{ 
+    FAST_INP;
+//    #ifndef ONLINE_JUDGE
+//    freopen("input.txt","r", stdin);
+//    freopen("output.txt","w", stdout);
+//    #endif
+    
+    int tc; cin >> tc;
+    TC(tc) solve();
+
+    return 0; 
+} 
