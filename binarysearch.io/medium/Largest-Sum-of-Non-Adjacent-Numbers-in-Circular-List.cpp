654de009b88ae962c8647dff9a686f54458f5265
wingkwong <wingkwong.code@gmail.com>
2020-09-06 19:35:59 +0800

    Update Largest-Sum-of-Non-Adjacent-Numbers-in-Circular-List.cpp

--- binarysearch.io/medium/Largest-Sum-of-Non-Adjacent-Numbers-in-Circular-List.cpp
+++ binarysearch.io/medium/Largest-Sum-of-Non-Adjacent-Numbers-in-Circular-List.cpp
@@ -1,3 +1,24 @@
+/*
+Largest Sum of Non-Adjacent Numbers in Circular List
+https://binarysearch.io/problems/Largest-Sum-of-Non-Adjacent-Numbers-in-Circular-List
+
+You are given a list of integers nums representing a circular list (the first and the last elements are adjacent). Return the largest sum of non-adjacent numbers.
+
+Constraints
+
+n ≤ 100,000 where n is the length of nums.
+Example 1
+Input
+
+nums = [9, 2, 3, 5]
+Output
+
+12
+Explanation
+
+We can take 9 and 3. Note that we can't take 9 and 5 since they are adjacent.
+*/
+
 #include "solution.hpp"
 using namespace std;
 
@@ -15,7 +36,7 @@ class Solution {
     }
     int solve(vector<int>& nums) {
         int n = nums.size();
-        if(n<2) return n?nums[0]:0;
+        if(n<2) return 0;
         return max(solve(nums,0,n-2), solve(nums,1,n-1));
     }
 };
