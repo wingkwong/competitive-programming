18dd241b1cd1a4620dac1508f395f1999fafd8f6
wingkwong <wingkwong.code@gmail.com>
2020-09-11 21:35:43 +0800

    Create template.cpp

---
+++ topcoder/contests/template.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+
+class CLASS_NAME {
+	public:
+	vector <string> func(int a, int b)
+	{
+	    FAST_INP;
+		
+	    return ans;
+	}
+};
+
+
+//int main(){
+//	CLASS_NAME c;
+//	c.func(1, 10);
+//	return 0;
+//}
