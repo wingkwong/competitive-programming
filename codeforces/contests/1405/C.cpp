ee77447860e601e3e20e0fe3aa50c42039f824a8
wingkwong <wingkwong.code@gmail.com>
2020-09-11 22:04:45 +0800

    Add Codeforces Round #668 (Div. 2)

---
+++ codeforces/contests/1405/C.cpp
@@ -0,0 +1,101 @@
+/*
+Codeforces Round #668 (Div. 2) - C. Balanced Bitstring
+https://codeforces.com/contest/1405/problem/C
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<string, string> pss;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+
+double EPS=1e-9;
+int INF=1000000005;
+long long INFF=1000000000000000005ll;
+double PI=acos(-1);
+int dirx[8]={ -1, 0, 0, 1, -1, -1, 1, 1 };
+int diry[8]={ 0, 1, -1, 0, -1, 1, -1, 1 };
+const ll MOD = 1000000007;
+
+#define DEBUG fprintf(stderr, "====TESTING====\n")
+#define VALUE(x) cerr << "The value of " << #x << " is " << x << endl
+#define OUT(x) cout << x << endl
+#define OUTH(x) cout << x << " "
+#define debug(...) fprintf(stderr, __VA_ARGS__)
+#define READ(x) for(auto &(z):x) cin >> z;
+#define FOR(a, b, c) for (int(a)=(b); (a) < (c); ++(a))
+#define FORN(a, b, c) for (int(a)=(b); (a) <= (c); ++(a))
+#define FORD(a, b, c) for (int(a)=(b); (a) >= (c); --(a))
+#define FORSQ(a, b, c) for (int(a)=(b); (a) * (a) <= (c); ++(a))
+#define FORC(a, b, c) for (char(a)=(b); (a) <= (c); ++(a))
+#define EACH(a, b) for (auto&(a) : (b))
+#define REP(i, n) FOR(i, 0, n)
+#define REPN(i, n) FORN(i, 1, n)
+#define MAX(a, b) a=max(a, b)
+#define MIN(a, b) a=min(a, b)
+#define SQR(x) ((ll)(x) * (x))
+#define RESET(a, b) memset(a, b, sizeof(a))
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define ALL(v) v.begin(), v.end()
+#define ALLA(arr, sz) arr, arr + sz
+#define SIZE(v) (int)v.size()
+#define SORT(v) sort(ALL(v))
+#define REVERSE(v) reverse(ALL(v))
+#define SORTA(arr, sz) sort(ALLA(arr, sz))
+#define REVERSEA(arr, sz) reverse(ALLA(arr, sz))
+#define PERMUTE next_permutation
+#define TC(t) while (t--)
+#define FAST_INP  ios_base::sync_with_stdio(false);cin.tie(NULL)
+
+
+void solve() {
+	int n, k;
+	cin >> n >> k;
+	string s;
+	cin >> s;
+	int ok=1;
+	vi zero(k,0), one(k,0);
+	// s[i] must be same as s[i+k]
+	// check if both 0 and 1 at [i+k] is set or not
+	// check the number of zero / the number of one * 2 > k or not
+	REP(i,s.size()){
+		if(s[i]=='0') zero[i%k]=1;
+		if(s[i]=='1') one[i%k]=1;
+	}
+	int zeroCnt=0, oneCnt=0;
+	REP(i,k){
+		if(zero[i]&&one[i]){
+			ok=0;
+			break;
+		}
+		if(zero[i]) zeroCnt++;
+		if(one[i]) oneCnt++;
+	}
+	if(zeroCnt*2>k||oneCnt*2>k) ok=0;
+	if(ok) OUT("YES");
+	else OUT("NO");
+}
+
+int main()
+{
+    FAST_INP;
+//    #ifndef ONLINE_JUDGE
+//    freopen("input.txt","r", stdin);
+//    freopen("output.txt","w", stdout);
+//    #endif
+
+    int tc; cin >> tc;
+    TC(tc) solve();
+
+    return 0;
+}
---
+++ codeforces/contests/1405/README.md
@@ -0,0 +1,7 @@
+# [Codeforces Round #668 (Div. 2)](https://codeforces.com/contest/1405)
+
+- [A. Permutation Forgery](https://github.com/wingkwong/competitive-programming/blob/master/codeforces/contests/1405/A.cpp)
+- [B. Array Cancellation](https://github.com/wingkwong/competitive-programming/blob/master/codeforces/contests/1405/B.cpp)
+- [C. Balanced Bitstring](https://github.com/wingkwong/competitive-programming/blob/master/codeforces/contests/1405/C.cpp)
+- D. Tree Tag
+- E. Fixed Point Removal
