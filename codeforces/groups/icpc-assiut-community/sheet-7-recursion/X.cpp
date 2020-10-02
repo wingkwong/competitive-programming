7cbdb491a83cb1dc2c061f98e128ec5cd025680e
wingkwong <wingkwong.code@gmail.com>
2020-10-02 13:17:00 +0800

    Add Assiut University Training - Newcomers - Sheet #7 (Recursion)

---
+++ codeforces/groups/icpc-assiut-community/sheet-7-recursion/X.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<string, string> pss;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+
+double EPS=1e-9;
+int INF=1000000005;
+long long INFF=1000000000000000005ll;
+double PI=acos(-1);
+int dirx[8]={ -1, 0, 0, 1, -1, -1, 1, 1 };
+int diry[8]={ 0, 1, -1, 0, -1, 1, -1, 1 };
+const ll MOD = 1000000007;
+
+ll sum() { return 0; }
+template<typename T, typename... Args>
+T sum(T a, Args... args) { return a + sum(args...); }
+
+#define DEBUG fprintf(stderr, "====TESTING====\n")
+#define VALUE(x) cerr << "The value of " << #x << " is " << x << endl
+#define OUT(x) cout << x << endl
+#define OUTH(x) cout << x << " "
+#define debug(...) fprintf(stderr, __VA_ARGS__)
+#define READ(x) for(auto &(z):x) cin >> z;
+#define FOR(a, b, c) for (int(a)=(b); (a) < (c); ++(a))
+#define FORN(a, b, c) for (int(a)=(b); (a) <= (c); ++(a))
+#define FORD(a, b, c) for (int(a)=(b); (a) >= (c); --(a))
+#define FORSQ(a, b, c) for (int(a)=(b); (a) * (a) <= (c); ++(a))
+#define FORC(a, b, c) for (char(a)=(b); (a) <= (c); ++(a))
+#define EACH(a, b) for (auto&(a) : (b))
+#define REP(i, n) FOR(i, 0, n)
+#define REPN(i, n) FORN(i, 1, n)
+#define MAX(a, b) a=max(a, b)
+#define MIN(a, b) a=min(a, b)
+#define SQR(x) ((ll)(x) * (x))
+#define RESET(a, b) memset(a, b, sizeof(a))
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define ALL(v) v.begin(), v.end()
+#define ALLA(arr, sz) arr, arr + sz
+#define SIZE(v) (int)v.size()
+#define SORT(v) sort(ALL(v))
+#define REVERSE(v) reverse(ALL(v))
+#define SORTA(arr, sz) sort(ALLA(arr, sz))
+#define REVERSEA(arr, sz) reverse(ALLA(arr, sz))
+#define PERMUTE next_permutation
+#define TC(t) while (t--)
+#define FAST_INP  ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define what_is(x) cerr << #x << " is " << x << endl;
+vvi dp(10,vi(10,INT_MIN));
+int solve(vvi& a, int i, int j) {
+	int n = a.size(), m = a[0].size();
+	if(i==n-1&&j==m-1) return a[i][j];
+	if(dp[i][j]!=INT_MIN) return dp[i][j];
+	int sum = dp[i][j];
+	if(i<n-1&&j<m-1) {
+		sum = a[i][j] + max({
+			solve(a,i,j+1),
+			solve(a,i+1,j)
+		});
+	} else if(i==n-1){
+		sum = a[i][j] + solve(a,i,j+1);
+	} else {
+		sum = a[i][j] + solve(a,i+1,j);
+	}
+	return dp[i][j]=sum;
+}
+
+int main()
+{
+    FAST_INP;
+//    #ifndef ONLINE_JUDGE
+//    freopen("input.txt","r", stdin);
+//    freopen("output.txt","w", stdout);
+//    #endif
+
+//    int tc; cin >> tc;
+//    TC(tc) solve();
+	int n, m; cin >> n >> m;
+	vvi a(n, vi(m));
+	REP(i,n) REP(j,m) cin >> a[i][j];
+	OUT(solve(a,0,0));
+    return 0;
+}
