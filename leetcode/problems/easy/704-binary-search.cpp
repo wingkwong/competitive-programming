758d1d6736924e296fd1d29ca24d6c3f0775a4a9
wingkwong <wingkwong.code@gmail.com>
2020-10-08 23:18:43 +0800

    Update 704-binary-search.cpp

--- leetcode/problems/easy/704-binary-search.cpp
+++ leetcode/problems/easy/704-binary-search.cpp
@@ -37,3 +37,17 @@ public:
         return r<n&&nums[r]==target?r:-1;
     }
 };
+
+class Solution2 {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0, r = nums.size() - 1;
+        while(l<=r) {
+            int mid = l + (r-l)/2;
+            if(nums[mid]==target) return mid;
+            else if(nums[mid]<target) l = mid + 1;
+            else r = mid - 1;
+        }
+        return -1;
+    }
+};
