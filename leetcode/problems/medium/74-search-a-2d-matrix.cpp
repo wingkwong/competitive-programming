a3cb8a1f904c04bef001b779d93e7589629123c5
wingkwong <wingkwong.code@gmail.com>
2020-10-16 21:41:17 +0800

    Create 74-search-a-2d-matrix.cpp

---
+++ leetcode/problems/medium/74-search-a-2d-matrix.cpp
@@ -0,0 +1,56 @@
+/*
+Search a 2D Matrix
+https://leetcode.com/problems/search-a-2d-matrix/
+
+Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+
+Integers in each row are sorted from left to right.
+The first integer of each row is greater than the last integer of the previous row.
+ 
+
+Example 1:
+
+
+Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,50]], target = 3
+Output: true
+Example 2:
+
+
+Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,50]], target = 13
+Output: false
+Example 3:
+
+Input: matrix = [], target = 0
+Output: false
+ 
+
+Constraints:
+
+m == matrix.length
+n == matrix[i].length
+0 <= m, n <= 100
+-104 <= matrix[i][j], target <= 104
+*/
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        if(m == 0) return false;
+        int n = matrix[0].size();
+        if(n == 0) return false;
+        int j = 0;
+        for(int i = 0; i < m; i++) {
+            if(target >= matrix[i][0] && target <= matrix[i][n-1]) {
+                j = i;
+                break;
+            }
+        }
+        for(int i = 0; i < n; i++){
+            if(matrix[j][i] == target) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
