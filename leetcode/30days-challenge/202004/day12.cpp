659ed847d1d0854a4eb4d3102b5e19369dbf2fde
wingkwong <wingkwong.code@gmail.com>
2020-10-22 00:02:51 +0800

    Update day12.cpp

--- leetcode/30days-challenge/202004/day12.cpp
+++ leetcode/30days-challenge/202004/day12.cpp
@@ -45,4 +45,17 @@ public:
     }
 };
 
+class Solution2 {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> pq(stones.begin(), stones.end());
+        while(pq.size() > 1) {
+            int x = pq.top(); pq.pop();
+            int y = pq.top(); pq.pop();
+            if(x > y) pq.push(x - y);
+        }
+        return pq.size() == 0 ? 0 : pq.top();
+    }
+};
+
 static const auto io_sync_off = []() {std::ios::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);return 0;}();
