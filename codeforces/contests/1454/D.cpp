4c67fe72199570ecf49f5f1582c874cf2d1a80cd
wingkwong <wingkwong.code@gmail.com>
2020-11-25 21:24:03 +0800

    Codeforces Round #686 (Div. 3) A - D

---
+++ codeforces/contests/1454/D.cpp
@@ -0,0 +1,157 @@
+/*
+Codeforces Round #686 (Div. 3) - D. Number into Sequence
+https://codeforces.com/contest/1454/problem/D
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<string, string> pss;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+
+double EPS=1e-9;
+int INF=1000000005;
+long long INFF=1000000000000000005ll;
+double PI=acos(-1);
+int dirx[8]={ -1, 0, 0, 1, -1, -1, 1, 1 };
+int diry[8]={ 0, 1, -1, 0, -1, 1, -1, 1 };
+const ll MOD = 1000000007;
+
+ll sum() { return 0; }
+template<typename T, typename... Args>
+T sum(T a, Args... args) { return a + sum(args...); }
+
+#define DEBUG fprintf(stderr, "====TESTING====\n")
+#define VALUE(x) cerr << "The value of " << #x << " is " << x << endl
+#define OUT(x) cout << x << endl
+#define OUTH(x) cout << x << " "
+#define debug(...) fprintf(stderr, __VA_ARGS__)
+#define READ(x) for(auto &(z):x) cin >> z;
+#define FOR(a, b, c) for (int(a)=(b); (a) < (c); ++(a))
+#define FORN(a, b, c) for (int(a)=(b); (a) <= (c); ++(a))
+#define FORD(a, b, c) for (int(a)=(b); (a) >= (c); --(a))
+#define FORSQ(a, b, c) for (int(a)=(b); (a) * (a) <= (c); ++(a))
+#define FORC(a, b, c) for (char(a)=(b); (a) <= (c); ++(a))
+#define EACH(a, b) for (auto&(a) : (b))
+#define REP(i, n) FOR(i, 0, n)
+#define REPN(i, n) FORN(i, 1, n)
+#define MAX(a, b) a=max(a, b)
+#define MIN(a, b) a=min(a, b)
+#define SQR(x) ((ll)(x) * (x))
+#define RESET(a, b) memset(a, b, sizeof(a))
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define ALL(v) v.begin(), v.end()
+#define ALLA(arr, sz) arr, arr + sz
+#define SIZE(v) (int)v.size()
+#define SORT(v) sort(ALL(v))
+#define REVERSE(v) reverse(ALL(v))
+#define SORTA(arr, sz) sort(ALLA(arr, sz))
+#define REVERSEA(arr, sz) reverse(ALLA(arr, sz))
+#define PERMUTE next_permutation
+#define TC(t) while (t--)
+#define FAST_INP  ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define what_is(x) cerr << #x << " is " << x << endl;
+
+const int mxN = 100005;
+
+vector<int> smallest_factor;
+vector<bool> prime;
+vector<int> primes;
+
+void sieve(int maximum) {
+    maximum = max(maximum, 1);
+    smallest_factor.assign(maximum + 1, 0);
+    prime.assign(maximum + 1, true);
+    prime[0] = prime[1] = false;
+    primes = {};
+    for (int p = 2; p <= maximum; p++)
+        if (prime[p]) {
+            smallest_factor[p] = p;
+            primes.push_back(p);
+            for (int64_t i = int64_t(p) * p; i <= maximum; i += p)
+                if (prime[i]) {
+                    prime[i] = false;
+                    smallest_factor[i] = p;
+                }
+        }
+}
+
+// Prime factorizes n in worst case O(sqrt n / log n). Requires having run `sieve` up to at least sqrt(n).
+// If we've run `sieve` up to at least n, takes O(log n) time.
+vector<pair<int64_t, int>> prime_factorize(int64_t n) {
+    int64_t sieve_max = int64_t(smallest_factor.size()) - 1;
+    assert(1 <= n && n <= sieve_max * sieve_max);
+    vector<pair<int64_t, int>> result;
+    if (n <= sieve_max) {
+        while (n != 1) {
+            int64_t p = smallest_factor[n];
+            int exponent = 0;
+            do {
+                n /= p;
+                exponent++;
+            } while (n % p == 0);
+            result.emplace_back(p, exponent);
+        }
+        return result;
+    }
+
+    for (int64_t p : primes) {
+        if (p * p > n)
+            break;
+        if (n % p == 0) {
+            result.emplace_back(p, 0);
+            do {
+                n /= p;
+                result.back().second++;
+            } while (n % p == 0);
+        }
+    }
+
+    if (n > 1)
+        result.emplace_back(n, 1);
+
+    return result;
+}
+
+void solve() {
+	ll n; cin >> n;
+	vector<pair<ll, int>> prime_factors = prime_factorize(n);
+	ll p = -1, mx = 0;
+	EACH(pf, prime_factors) {
+		if(pf.se > mx) {
+			mx = pf.se;
+			p = pf.fi;
+		}
+	}
+	OUT(mx);
+	vl a(mx, p);
+	while(n % p == 0) n /= p;
+	a.back() *= n;
+	EACH(x, a) OUTH(x);
+	OUT("");
+}
+
+int main()
+{
+    FAST_INP;
+//    #ifndef ONLINE_JUDGE
+//    freopen("input.txt","r", stdin);
+//    freopen("output.txt","w", stdout);
+//    #endif
+
+    int tc; cin >> tc;
+    sieve(mxN);
+    TC(tc) solve();
+
+    return 0;
+}
