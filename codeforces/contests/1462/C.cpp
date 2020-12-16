7cb2ee529f913a5693238d911503f26f5f1468c3
wingkwong <wingkwong.code@gmail.com>
2020-12-16 23:39:28 +0800

    Codeforces Round #690 (Div. 3)

---
+++ codeforces/contests/1462/C.cpp
@@ -0,0 +1,111 @@
+/*
+Codeforces Round #690 (Div. 3) - C - Unique Number
+https://codeforces.com/contest/1462/problem/C
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<string, string> pss;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+
+double EPS=1e-9;
+int INF=1000000005;
+long long INFF=1000000000000000005ll;
+double PI=acos(-1);
+int dirx[8]={ -1, 0, 0, 1, -1, -1, 1, 1 };
+int diry[8]={ 0, 1, -1, 0, -1, 1, -1, 1 };
+const ll MOD = 1000000007;
+
+ll sum() { return 0; }
+template<typename T, typename... Args>
+T sum(T a, Args... args) { return a + sum(args...); }
+
+#define DEBUG fprintf(stderr, "====TESTING====\n")
+#define VALUE(x) cerr << "The value of " << #x << " is " << x << endl
+#define OUT(x) cout << x << endl
+#define OUTH(x) cout << x
+#define debug(...) fprintf(stderr, __VA_ARGS__)
+#define READ(x) for(auto &(z):x) cin >> z;
+#define FOR(a, b, c) for (int(a)=(b); (a) < (c); ++(a))
+#define FORN(a, b, c) for (int(a)=(b); (a) <= (c); ++(a))
+#define FORD(a, b, c) for (int(a)=(b); (a) >= (c); --(a))
+#define FORSQ(a, b, c) for (int(a)=(b); (a) * (a) <= (c); ++(a))
+#define FORC(a, b, c) for (char(a)=(b); (a) <= (c); ++(a))
+#define EACH(a, b) for (auto&(a) : (b))
+#define REP(i, n) FOR(i, 0, n)
+#define REPN(i, n) FORN(i, 1, n)
+#define MAX(a, b) a=max(a, b)
+#define MIN(a, b) a=min(a, b)
+#define SQR(x) ((ll)(x) * (x))
+#define RESET(a, b) memset(a, b, sizeof(a))
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define ALL(v) v.begin(), v.end()
+#define ALLA(arr, sz) arr, arr + sz
+#define SIZE(v) (int)v.size()
+#define SORT(v) sort(ALL(v))
+#define REVERSE(v) reverse(ALL(v))
+#define SORTA(arr, sz) sort(ALLA(arr, sz))
+#define REVERSEA(arr, sz) reverse(ALLA(arr, sz))
+#define PERMUTE next_permutation
+#define TC(t) while (t--)
+#define FAST_INP  ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define what_is(x) cerr << #x << " is " << x << endl;
+
+// WA
+//void solve() {
+//	ll ans = ((x % 9 + 1) * pow(10, (x / 9)) - 1);
+//	string s = to_string(ans);
+//	int cnt[10] = {0};
+//	REP(i, s.size()) cnt[s[i] - '0']++;
+//	int ok = 1;
+//	REP(i, 10) {
+//		if(cnt[i] > 1) {
+//			ok = 0;
+//			break;
+//		}
+//	}
+//	if(ok) OUT(ans);
+//	else OUT(-1);
+//}
+
+void solve() {
+	int x; cin >> x;
+	int sum = 0, last = 9;
+	vi ans;
+	while(sum < x && last > 0) {
+		ans.pb(min(x - sum, last));
+		sum += last--;
+	}
+	if(sum < x) {
+		OUT(-1);
+	} else {
+		int n = (int) ans.size();
+		FORD(i, n - 1, 0) OUTH(ans[i]);
+	}
+    OUT("");
+}
+
+int main()
+{
+    FAST_INP;
+//    #ifndef ONLINE_JUDGE
+//    freopen("input.txt","r", stdin);
+//    freopen("output.txt","w", stdout);
+//    #endif
+
+    int tc; cin >> tc;
+    TC(tc) solve();
+
+    return 0;
+}
