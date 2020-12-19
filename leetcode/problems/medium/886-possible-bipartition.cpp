ca62c37d64c936739332a99e112c3cf837a4c924
wingkwong <wingkwong.code@gmail.com>
2020-12-19 15:17:09 +0800

    Update 886-possible-bipartition.cpp

--- leetcode/problems/medium/886-possible-bipartition.cpp
+++ leetcode/problems/medium/886-possible-bipartition.cpp
@@ -34,6 +34,72 @@ There does not exist i != j for which dislikes[i] == dislikes[j].
 
 class Solution {
 public:
+    struct check_bipartite {
+        int V;
+        vector<vector<int>> adj;
+        vector<int> depth;
+        vector<bool> visited;
+
+        check_bipartite(int v = -1) {
+            if (v >= 0)
+                init(v);
+        }
+
+        void init(int v) {
+            V = v;
+            adj.assign(V, {});
+        }
+
+        void add_edge(int a, int b) {
+            adj[a].push_back(b);
+            adj[b].push_back(a);
+        }
+
+        vector<array<vector<int>, 2>> components;
+
+        bool dfs(int node, int parent) {
+            assert(!visited[node]);
+            visited[node] = true;
+            depth[node] = parent < 0 ? 0 : depth[parent] + 1;
+            components.back()[depth[node] % 2].push_back(node);
+
+            for (int neigh : adj[node])
+                if (neigh != parent) {
+                    if (!visited[neigh] && !dfs(neigh, node))
+                        return false;
+
+                    if (depth[node] % 2 == depth[neigh] % 2)
+                        return false;
+                }
+
+            return true;
+        }
+        
+        bool solve() {
+            depth.assign(V, -1);
+            visited.assign(V, false);
+            components = {};
+
+            for (int i = 0; i < V; i++)
+                if (!visited[i]) {
+                    components.emplace_back();
+
+                    if (!dfs(i, -1))
+                        return false;
+                }
+
+            return true;
+        }
+    };
+    bool possibleBipartition(int N, vector<vector<int>>& dislikes) {
+        check_bipartite c(N);
+        for(auto d : dislikes) c.add_edge(--d[0], --d[1]);
+        return c.solve();
+    }
+};
+
+class Solution2 {
+public:
     bool possibleBipartition(int N, vector<vector<int>>& dislikes) {
         vector<vector<int>> graph(N,vector<int>(N,0));
         vector<int> group(N,0);
