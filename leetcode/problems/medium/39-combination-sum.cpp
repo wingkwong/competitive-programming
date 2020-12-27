39b85b7eb44dcde9a8185dca564713fe90aed012
wingkwong <wingkwong.code@gmail.com>
2020-12-27 14:54:40 +0800

    Update 39-combination-sum.cpp

--- leetcode/problems/medium/39-combination-sum.cpp
+++ leetcode/problems/medium/39-combination-sum.cpp
@@ -51,11 +51,11 @@ void backtrack(vector<int>& candidates, int target, vector<vector<int>>& ans, ve
         }
         for(int i = start; i < candidates.size() && target >= candidates[i]; i++){
             tmp.push_back(candidates[i]);
-            backtrack(candidates, target-candidates[i], ans, tmp, i);
+            backtrack(candidates, target - candidates[i], ans, tmp, i);
             tmp.pop_back();
         }
     }
-    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         sort(candidates.begin(), candidates.end());
         vector<vector<int>> ans;
         vector<int> tmp;
