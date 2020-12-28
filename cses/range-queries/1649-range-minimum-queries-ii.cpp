1b5d128afb87830d1becc876b2274995b9cd1db3
wingkwong <wingkwong.code@gmail.com>
2020-12-28 20:04:25 +0800

    Create 1649-range-minimum-queries-ii.cpp

---
+++ cses/range-queries/1649-range-minimum-queries-ii.cpp
@@ -0,0 +1,233 @@
+/*
+Range Minimum Queries II
+https://cses.fi/problemset/task/1649
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<string, string> pss;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+
+double EPS=1e-9;
+int INF=1000000005;
+long long INFF=1000000000000000005ll;
+double PI=acos(-1);
+int dirx[8]={ -1, 0, 0, 1, -1, -1, 1, 1 };
+int diry[8]={ 0, 1, -1, 0, -1, 1, -1, 1 };
+// const ll MOD = 1000000007;
+
+// ll sum() { return 0; }
+// template<typename T, typename... Args>
+// T sum(T a, Args... args) { return a + sum(args...); }
+
+#define DEBUG fprintf(stderr, "====TESTING====\n")
+#define VALUE(x) cerr << "The value of " << #x << " is " << x << endl
+#define OUT(x) cout << x << endl
+#define OUTH(x) cout << x << " "
+#define debug(...) fprintf(stderr, __VA_ARGS__)
+#define READ(x) for(auto &(z):x) cin >> z;
+#define FOR(a, b, c) for (int(a)=(b); (a) < (c); ++(a))
+#define FORN(a, b, c) for (int(a)=(b); (a) <= (c); ++(a))
+#define FORD(a, b, c) for (int(a)=(b); (a) >= (c); --(a))
+#define FORSQ(a, b, c) for (int(a)=(b); (a) * (a) <= (c); ++(a))
+#define FORC(a, b, c) for (char(a)=(b); (a) <= (c); ++(a))
+#define EACH(a, b) for (auto&(a) : (b))
+#define REP(i, n) FOR(i, 0, n)
+#define REPN(i, n) FORN(i, 1, n)
+#define MAX(a, b) a=max(a, b)
+#define MIN(a, b) a=min(a, b)
+#define SQR(x) ((ll)(x) * (x))
+#define RESET(a, b) memset(a, b, sizeof(a))
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define ALL(v) v.begin(), v.end()
+#define ALLA(arr, sz) arr, arr + sz
+#define SIZE(v) (int)v.size()
+#define SORT(v) sort(ALL(v))
+#define REVERSE(v) reverse(ALL(v))
+#define SORTA(arr, sz) sort(ALLA(arr, sz))
+#define REVERSEA(arr, sz) reverse(ALLA(arr, sz))
+#define PERMUTE next_permutation
+#define TC(t) while (t--)
+#define FAST_INP  ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define what_is(x) cerr << #x << " is " << x << endl;
+
+
+// SEGMENTTREE template starts
+// https://github.com/kartik8800/segTree/blob/master/SegmentTree.h
+#define left(i) (2*i + 1)
+#define right(i) (2*i + 2)
+#define parent(i) ((i-1)/2)
+ 
+template<class T>
+class SegmentTree
+{
+    public:
+        //tree constructors.
+        SegmentTree(std::vector<T> data, T value, T (*combine)(T obj1, T obj2));
+        SegmentTree(T ar[], int n, T value, T (*combine)(T obj1, T obj2));
+ 
+        //query the range l to r, 0 based array indexing.
+        T query(int l, int r);
+ 
+        //update the element at index idx to val.
+        void update(int idx, T val);
+        ///TODO lazy propagation
+    private:
+        //represents the segment tree.
+        T *tree;
+ 
+        //builds the segment tree.
+        void buildTree(std::vector<T> data);
+ 
+        //size of the segment tree array.
+        int segTreeSize;
+ 
+        //extra nodes must be added to array to make its size a power of 2
+        //this is the value to be filled for the those nodes.
+        T valueForExtraNodes;
+ 
+        //specifies how to combine child node results to form parent node result.
+        T (*combine)(T obj1, T obj2);
+ 
+        //used to calculate the size of array needed to store the tree.
+        int calculateSize(int n);
+ 
+        //helps to solve a range query.
+        T queryHelper(int l,int r, int st, int ed, int node);
+};
+ 
+template<class T> SegmentTree<T>::SegmentTree(std::vector<T> data,
+                                                T value, T (*combine)(T obj1, T obj2))
+{
+   this->combine = combine;
+   valueForExtraNodes = value;
+   segTreeSize = calculateSize(data.size());
+   buildTree(data);
+}
+ 
+template<class T> SegmentTree<T>::SegmentTree(T ar[], int n,
+                                            T value, T (*combine)(T obj1, T obj2))
+{
+   this->combine = combine;
+   valueForExtraNodes = value;
+   segTreeSize = calculateSize(n);
+ 
+   std::vector<T> data;
+   for(int i = 0; i < n; i++)
+         data.push_back(ar[i]);
+ 
+   buildTree(data);
+}
+ 
+ 
+template<class T> int SegmentTree<T>::calculateSize(int n)
+{
+    int pow2 = 1;
+    while( pow2 < n)
+    {
+        pow2 = pow2 << 1;
+    }
+    return 2*pow2 - 1;
+}
+ 
+template<class T> T SegmentTree<T>::query(int l, int r)
+{
+    int st = 0, ed = segTreeSize/2;
+    return queryHelper(l, r, st, ed, 0);
+}
+ 
+template<class T> T SegmentTree<T>::queryHelper(int l,int r, int st, int ed, int node)
+{
+    if( (r < st) || (l > ed) || (l > r) )
+        return valueForExtraNodes;
+    if(st >= l && ed <= r)
+        return tree[node];
+    T leftVal = queryHelper(l, r, st, (st + ed)/2, left(node));
+    T rightVal = queryHelper(l, r, (st+ed)/2 + 1, ed, right(node));
+    return combine(leftVal, rightVal);
+}
+ 
+template<class T> void SegmentTree<T>::buildTree(std::vector<T> data)
+{
+   int n = data.size();
+   tree = new T[segTreeSize];
+   int extraNodes = (segTreeSize/2 + 1) - n;
+   for(int i = segTreeSize - 1; i >= 0; i--)
+   {
+       if(extraNodes>0)
+           {
+               tree[i] = valueForExtraNodes;
+               extraNodes--;
+           }
+       else if(n>0)
+           {
+               tree[i] = data[n-1];
+               n--;
+           }
+       else
+           tree[i] = combine(tree[left(i)], tree[right(i)]);
+   }
+}
+ 
+template<class T> void SegmentTree<T>::update(int idx, T val)
+{
+    int segTreeIdx = (segTreeSize/2) + idx;
+    tree[segTreeIdx] = val;
+    while(parent(segTreeIdx) >= 0)
+    {
+        segTreeIdx = parent(segTreeIdx);
+        if(right(segTreeIdx) < segTreeSize)
+          tree[segTreeIdx] = combine(tree[left(segTreeIdx)], tree[right(segTreeIdx)]);
+        if(segTreeIdx == 0)
+            break;
+    }
+}
+// SEGMENTTREE template ends
+
+ll seg_func(ll x, ll y) {
+  return min(x, y);
+}
+
+void solve() {
+	ll n, q; 
+	cin >> n >> q;
+	vector<ll> x(n);
+	READ(x);
+	SegmentTree<ll> sg(x, INT_MAX, seg_func);
+	TC(q) {
+		ll k, a, b;
+		cin >> k >> a >> b;
+    if(k == 1) {
+      --a;
+      sg.update(a, b);
+    } else {
+      --a, --b;
+      OUT(sg.query(a, b));
+    }
+	}
+}
+
+int main()
+{
+    FAST_INP;
+//    #ifndef ONLINE_JUDGE
+//    freopen("input.txt","r", stdin);
+//    freopen("output.txt","w", stdout);
+//    #endif
+
+    // int tc; cin >> tc;
+    // TC(tc) solve();
+    solve();
+    return 0;
+}
