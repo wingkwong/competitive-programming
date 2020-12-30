a4deb7fac1c9e44e9c9cff3c56ccc2941f2f4e2f
wingkwong <wingkwong.code@gmail.com>
2020-12-30 13:21:37 +0800

    Create 1746-array-description.cpp

---
+++ cses/dynamic-programming/1746-array-description.cpp
@@ -0,0 +1,110 @@
+/*
+Array Description
+https://cses.fi/problemset/task/1746
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<string, string> pss;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+
+double EPS=1e-9;
+int INF=1000000005;
+long long INFF=1000000000000000005ll;
+double PI=acos(-1);
+int dirx[8]={ -1, 0, 0, 1, -1, -1, 1, 1 };
+int diry[8]={ 0, 1, -1, 0, -1, 1, -1, 1 };
+const ll MOD = 1000000007;
+
+// ll sum() { return 0; }
+// template<typename T, typename... Args>
+// T sum(T a, Args... args) { return a + sum(args...); }
+
+#define DEBUG fprintf(stderr, "====TESTING====\n")
+#define VALUE(x) cerr << "The value of " << #x << " is " << x << endl
+#define OUT(x) cout << x << endl
+#define OUTH(x) cout << x << " "
+#define debug(...) fprintf(stderr, __VA_ARGS__)
+#define READ(x) for(auto &(z):x) cin >> z;
+#define FOR(a, b, c) for (int(a)=(b); (a) < (c); ++(a))
+#define FORN(a, b, c) for (int(a)=(b); (a) <= (c); ++(a))
+#define FORD(a, b, c) for (int(a)=(b); (a) >= (c); --(a))
+#define FORSQ(a, b, c) for (int(a)=(b); (a) * (a) <= (c); ++(a))
+#define FORC(a, b, c) for (char(a)=(b); (a) <= (c); ++(a))
+#define EACH(a, b) for (auto&(a) : (b))
+#define REP(i, n) FOR(i, 0, n)
+#define REPN(i, n) FORN(i, 1, n)
+#define MAX(a, b) a=max(a, b)
+#define MIN(a, b) a=min(a, b)
+#define SQR(x) ((ll)(x) * (x))
+#define RESET(a, b) memset(a, b, sizeof(a))
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define ALL(v) v.begin(), v.end()
+#define ALLA(arr, sz) arr, arr + sz
+#define SIZE(v) (int)v.size()
+#define SORT(v) sort(ALL(v))
+#define REVERSE(v) reverse(ALL(v))
+#define SORTA(arr, sz) sort(ALLA(arr, sz))
+#define REVERSEA(arr, sz) reverse(ALLA(arr, sz))
+#define PERMUTE next_permutation
+#define TC(t) while (t--)
+#define FAST_INP  ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define what_is(x) cerr << #x << " is " << x << endl
+
+void solve() {
+  int n, m;
+  cin >> n >> m;
+  // dp[i][j] = number of ways to fill up the array up to index i where x[i] = v.
+  vvi dp(n, vi(m + 1, 0));
+  int v; cin >> v;
+  if(v == 0) fill(ALL(dp[0]), 1);
+  else dp[0][v] = 1;
+  FOR(i, 1, n) {
+    cin >> v;
+    if(v == 0) {
+      FORN(j, 1, m) {
+        // replace it by value either v - 1, v or v + 1 where j = 1 .. m
+        for(int k : {j - 1, j, j + 1}) {
+          if(k >= 1 && k <= m) {
+            dp[i][j] = (dp[i][j] + dp[i - 1][k]) % MOD; 
+          }
+        }
+      }
+    } else {
+        // replace it by value either v - 1, v or v + 1
+        for(int k : {v - 1, v, v + 1}) {
+          if(k >= 1 && k <= m) {
+            dp[i][v] = (dp[i][v] + dp[i - 1][k]) % MOD; 
+          }
+        }
+    }
+  }
+  int ans = 0;
+  FORN(j, 1, m) ans = (ans + dp[n - 1][j]) % MOD;
+  OUT(ans);
+}
+
+int main()
+{
+    FAST_INP;
+//    #ifndef ONLINE_JUDGE
+//    freopen("input.txt","r", stdin);
+//    freopen("output.txt","w", stdout);
+//    #endif
+
+    // int tc; cin >> tc;
+    // TC(tc) solve();
+    solve();
+    return 0;
+}
