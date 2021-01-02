e812dc0620a3e4a29fc0fdce273e37deda77997c
wingkwong <wingkwong.code@gmail.com>
2021-01-02 17:56:54 +0800

    Create 1678-round-trip-ii.cpp

---
+++ cses/graph-algorithms/1678-round-trip-ii.cpp
@@ -0,0 +1,123 @@
+/*
+Round Trip II
+https://cses.fi/problemset/task/1678
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<string, string> pss;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+
+double EPS=1e-9;
+int INF=1000000005;
+long long INFF=1000000000000000005ll;
+double PI=acos(-1);
+int dirx[8]={ -1, 0, 0, 1, -1, -1, 1, 1 };
+int diry[8]={ 0, 1, -1, 0, -1, 1, -1, 1 };
+const ll MOD = 1000000007;
+
+// ll sum() { return 0; }
+// template<typename T, typename... Args>
+// T sum(T a, Args... args) { return a + sum(args...); }
+
+#define DEBUG fprintf(stderr, "====TESTING====\n")
+#define VALUE(x) cerr << "The value of " << #x << " is " << x << endl
+#define OUT(x) cout << x << endl
+#define OUTH(x) cout << x << " "
+#define debug(...) fprintf(stderr, __VA_ARGS__)
+#define READ(x) for(auto &(z):x) cin >> z;
+#define FOR(a, b, c) for (int(a)=(b); (a) < (c); ++(a))
+#define FORN(a, b, c) for (int(a)=(b); (a) <= (c); ++(a))
+#define FORD(a, b, c) for (int(a)=(b); (a) >= (c); --(a))
+#define FORSQ(a, b, c) for (int(a)=(b); (a) * (a) <= (c); ++(a))
+#define FORC(a, b, c) for (char(a)=(b); (a) <= (c); ++(a))
+#define EACH(a, b) for (auto&(a) : (b))
+#define REP(i, n) FOR(i, 0, n)
+#define REPN(i, n) FORN(i, 1, n)
+#define MAX(a, b) a=max(a, b)
+#define MIN(a, b) a=min(a, b)
+#define SQR(x) ((ll)(x) * (x))
+#define RESET(a, b) memset(a, b, sizeof(a))
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define ALL(v) v.begin(), v.end()
+#define ALLA(arr, sz) arr, arr + sz
+#define SIZE(v) (int)v.size()
+#define SORT(v) sort(ALL(v))
+#define REVERSE(v) reverse(ALL(v))
+#define SORTA(arr, sz) sort(ALLA(arr, sz))
+#define REVERSEA(arr, sz) reverse(ALLA(arr, sz))
+#define PERMUTE next_permutation
+#define TC(t) while (t--)
+#define FAST_INP  ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define what_is(x) cerr << #x << " is " << x << endl
+
+const int mxN = 1e5;
+vi g[mxN], ans;
+int vis[mxN], par[mxN];
+
+void dfs(int u) {
+  vis[u] = 1;
+  for(int v : g[u]){
+      if(!vis[v]) {
+        par[v] = u;
+        dfs(v);
+      } else if(vis[v] == 1) {
+        // cycle found 
+        ans.pb(v);
+        while(u ^ v) {
+          // starting point : v
+          // backtrack each u
+          ans.pb(u);
+          u = par[u];
+        }
+        ans.pb(v);
+        REVERSE(ans);
+        OUT(SIZE(ans));
+        EACH(x, ans) OUTH(x + 1);
+        OUT("");
+        exit(0);
+      }
+  }
+  vis[u] = 2;
+}
+
+void solve() {
+  int n, m;
+  cin >> n >> m;
+  REP(i, m) {
+    int a, b;
+    cin >> a >> b;
+    --a, --b;
+    g[a].pb(b);
+    // g[b].pb(a);
+  }
+  REP(i, n) {
+    if(!vis[i]) dfs(i);
+  }
+  OUT("IMPOSSIBLE");
+}
+
+int main()
+{
+    FAST_INP;
+//    #ifndef ONLINE_JUDGE
+//    freopen("input.txt","r", stdin);
+//    freopen("output.txt","w", stdout);
+//    #endif
+
+    // int tc; cin >> tc;
+    // TC(tc) solve();
+    solve();
+    return 0;
+}
