5bebdf93e5b704b9aef56e4d19f3b707a2131e9b
wingkwong <wingkwong.code@gmail.com>
2021-01-03 14:14:04 +0800

    Update 1671-shortest-routes-i.cpp

--- cses/graph-algorithms/1671-shortest-routes-i.cpp
+++ cses/graph-algorithms/1671-shortest-routes-i.cpp
@@ -3,96 +3,113 @@ Shortest Routes I
 https://cses.fi/problemset/task/1671
 */
 
+
 #include <bits/stdc++.h>
-using namespace std; 
+using namespace std;
 
-typedef long long ll; 
-typedef pair<int, int> pii; 
-typedef pair<ll, ll> pll; 
-typedef pair<string, string> pss; 
-typedef vector<int> vi; 
-typedef vector<vi> vvi; 
-typedef vector<pii> vii; 
-typedef vector<pll> vll; 
-typedef vector<ll> vl; 
-typedef vector<vl> vvl; 
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<string, string> pss;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
 
-double EPS=1e-9; 
-int INF=1000000005; 
-ll INFF=1000000000000000005ll; 
-double PI=acos(-1); 
-int dirx[8]={ -1, 0, 0, 1, -1, -1, 1, 1 }; 
-int diry[8]={ 0, 1, -1, 0, -1, 1, -1, 1 }; 
+double EPS=1e-9;
+int INF=1000000005;
+long long INFF=1000000000000000005ll;
+double PI=acos(-1);
+int dirx[8]={ -1, 0, 0, 1, -1, -1, 1, 1 };
+int diry[8]={ 0, 1, -1, 0, -1, 1, -1, 1 };
 const ll MOD = 1000000007;
 
-#define DEBUG fprintf(stderr, "====TESTING====\n") 
-#define VALUE(x) cerr << "The value of " << #x << " is " << x << endl 
-#define OUT(x) cout << x << endl 
-#define debug(...) fprintf(stderr, __VA_ARGS__) 
-#define READ(b) for(auto &(a):b) cin >> a;
-#define FOR(a, b, c) for (int(a)=(b); (a) < (c); ++(a)) 
-#define FORN(a, b, c) for (int(a)=(b); (a) <= (c); ++(a)) 
-#define FORD(a, b, c) for (int(a)=(b); (a) >= (c); --(a)) 
-#define FORSQ(a, b, c) for (int(a)=(b); (a) * (a) <= (c); ++(a)) 
-#define FORC(a, b, c) for (char(a)=(b); (a) <= (c); ++(a)) 
-#define EACH(a, b) for (auto&(a) : (b)) 
-#define REP(i, n) FOR(i, 0, n) 
-#define REPN(i, n) FORN(i, 1, n) 
-#define MAX(a, b) a=max(a, b) 
-#define MIN(a, b) a=min(a, b) 
-#define SQR(x) ((ll)(x) * (x)) 
-#define RESET(a, b) memset(a, b, sizeof(a)) 
-#define fi first 
-#define se second 
-#define mp make_pair 
-#define pb push_back 
-#define ALL(v) v.begin(), v.end() 
-#define ALLA(arr, sz) arr, arr + sz 
-#define SIZE(v) (int)v.size() 
-#define SORT(v) sort(ALL(v)) 
-#define REVERSE(v) reverse(ALL(v)) 
-#define SORTA(arr, sz) sort(ALLA(arr, sz)) 
-#define REVERSEA(arr, sz) reverse(ALLA(arr, sz)) 
-#define PERMUTE next_permutation 
-#define TC(t) while (t--) 
-#define FAST_INP  ios_base::sync_with_stdio(false);cin.tie(NULL)
+// ll sum() { return 0; }
+// template<typename T, typename... Args>
+// T sum(T a, Args... args) { return a + sum(args...); }
 
-int main()  
-{ 
+#define DEBUG fprintf(stderr, "====TESTING====\n")
+#define VALUE(x) cerr << "The value of " << #x << " is " << x << endl
+#define OUT(x) cout << x << endl
+#define OUTH(x) cout << x << " "
+#define debug(...) fprintf(stderr, __VA_ARGS__)
+#define READ(x) for(auto &(z):x) cin >> z;
+#define FOR(a, b, c) for (int(a)=(b); (a) < (c); ++(a))
+#define FORN(a, b, c) for (int(a)=(b); (a) <= (c); ++(a))
+#define FORD(a, b, c) for (int(a)=(b); (a) >= (c); --(a))
+#define FORSQ(a, b, c) for (int(a)=(b); (a) * (a) <= (c); ++(a))
+#define FORC(a, b, c) for (char(a)=(b); (a) <= (c); ++(a))
+#define EACH(a, b) for (auto&(a) : (b))
+#define REP(i, n) FOR(i, 0, n)
+#define REPN(i, n) FORN(i, 1, n)
+#define MAX(a, b) a=max(a, b)
+#define MIN(a, b) a=min(a, b)
+#define SQR(x) ((ll)(x) * (x))
+#define RESET(a, b) memset(a, b, sizeof(a))
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define ALL(v) v.begin(), v.end()
+#define ALLA(arr, sz) arr, arr + sz
+#define SIZE(v) (int)v.size()
+#define SORT(v) sort(ALL(v))
+#define REVERSE(v) reverse(ALL(v))
+#define SORTA(arr, sz) sort(ALLA(arr, sz))
+#define REVERSEA(arr, sz) reverse(ALLA(arr, sz))
+#define PERMUTE next_permutation
+#define TC(t) while (t--)
+#define FAST_INP  ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define what_is(x) cerr << #x << " is " << x << endl
+
+const int mxN = 1e5;
+vector<pll> g[mxN], ans;
+ll n, m, dist[mxN];
+
+void dijkstra(int u) {
+  priority_queue<pll, vector<pll>, greater<pll>> pq;
+  pq.push({0, u}); 
+  memset(dist, INFF, sizeof(dist));
+  dist[u] = 0;
+  while (!pq.empty()) {
+      pll p = pq.top(); 
+      ll d = p.fi, u = p.se;
+      pq.pop();
+      if (d > dist[u]) continue;
+      for (pll p2 : g[u]) {
+          ll v = p2.fi, w = p2.se;
+          if (dist[u] + w < dist[v]) {
+            dist[v] = dist[u] + w;
+            pq.push({dist[v], v});
+          } 
+      }
+  } 
+}
+
+void solve() {
+  cin >> n >> m;
+  REP(i, m) {
+    int u, v, w;
+    cin >> u >> v >> w;
+    --u, --v;
+    g[u].pb({v, w});
+  }
+  dijkstra(0);
+  REP(i, n) OUTH(dist[i]);
+}
+
+int main()
+{
     FAST_INP;
-    // #ifndef ONLINE_JUDGE
-    // freopen("input.txt","r", stdin);
-    // freopen("output.txt","w", stdout);
-    // #endif
-    int n,m,a,b,c;
-    cin >> n >> m;
-    const int mxN=2e5;
-    vector<vii> adj(mxN);
-    // dijkstra with priority queue
-	priority_queue<pair<ll,ll>, vector<pair<ll,ll>>, greater<pair<ll,ll>>> pq;
-    REP(i,m){
-    	cin >> a >> b >> c;
-    	--a,--b;
-    	adj[a].pb({c,b});
-	}
-	vl dist(n,INFF);
-	pq.push({0,0}); // {dist, node}
-	dist[0]=0;
-	while(!pq.empty()){
-		ll d = pq.top().first;
-		ll u = pq.top().second;
-		pq.pop();
-		if(d>dist[u]) continue;
-		for(pair<ll,ll> p: adj[u]){
-			ll dd = p.first; // dist
-			ll v = p.second; // node
-			if(dist[v]>d+dd){
-				dist[v]=d+dd;
-				pq.push({d+dd,v});
-			}
-		}
-	}
-	REP(i,n) cout << dist[i] << " ";
-	OUT("");
-    return 0; 
-} 
+//    #ifndef ONLINE_JUDGE
+//    freopen("input.txt","r", stdin);
+//    freopen("output.txt","w", stdout);
+//    #endif
+
+    // int tc; cin >> tc;
+    // TC(tc) solve();
+    solve();
+    return 0;
+}
+
