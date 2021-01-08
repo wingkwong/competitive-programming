1e7c836dfb429bca33e7dee05547f2f7cc03eae1
wingkwong <wingkwong.code@gmail.com>
2021-01-08 19:36:07 +0800

    Update 300-longest-increasing-subsequence.cpp

--- leetcode/problems/medium/300-longest-increasing-subsequence.cpp
+++ leetcode/problems/medium/300-longest-increasing-subsequence.cpp
@@ -18,21 +18,34 @@ Follow up: Could you improve it to O(n log n) time complexity?
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
+        vector<int> lcs;
+        for(int n : nums) {
+            auto it = lower_bound(lcs.begin(), lcs.end(), n);
+            if(it == lcs.end()) lcs.push_back(n);
+            else *it = n;
+        }
+        return (int) lcs.size();
+    }
+};
+
+class Solution2 {
+public:
+    int lengthOfLIS(vector<int>& nums) {
         //  L(i) = 1 + max( L(j) ) where 0 < j < i and arr[j] < arr[i]; or
         //  L(i) = 1, if no such j exists.
-        int n=nums.size();
-        if(n==0) return 0;
-        vector<int> dp(n+1,1);
-        for(int i=1;i<n;i++){
-            for(int j=0;j<i;j++){
-                if(nums[i]>nums[j]){
-                    dp[i]=max(dp[i],dp[j]+1);
+        int n = nums.size();
+        if(n == 0) return 0;
+        vector<int> dp(n + 1, 1);
+        for(int i = 1; i < n; i++){
+            for(int j = 0; j < i; j++){
+                if(nums[i] > nums[j]){
+                    dp[i] = max(dp[i], dp[j] + 1);
                 }
             }
         }
         // find out the max in dp
         int ans=0;
-        for(int k:dp) ans=max(ans,k);
+        for(int k: dp) ans = max(ans, k);
         return ans;
         // or return *max_element(dp.begin(),dp.end());
     }
