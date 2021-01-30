704f3698ff581eeabde3659eea9a2f8a67907125
wingkwong <wingkwong.code@gmail.com>
2021-01-30 12:12:49 +0800

    USACO 2021 January Contest, Bronze

---
+++ usaco/contests/USACO 2021 January Contest/B.cpp
@@ -0,0 +1,113 @@
+/*
+USACO 2021 January Contest, Bronze
+Problem 2. Even More Odd Photos
+http://www.usaco.org/index.php?page=viewproblem2&cpid=1084
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<string, string> pss;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+
+double EPS=1e-9;
+int INF=1000000005;
+long long INFF=1000000000000000005ll;
+double PI=acos(-1);
+int dirx[8]={ -1, 0, 0, 1, -1, -1, 1, 1 };
+int diry[8]={ 0, 1, -1, 0, -1, 1, -1, 1 };
+ll MOD = 1000000007;
+
+// ll sum() { return 0; }
+// template<typename T, typename... Args>
+// T sum(T a, Args... args) { return a + sum(args...); }
+
+#define DEBUG fprintf(stderr, "====TESTING====\n")
+#define VALUE(x) cerr << "The value of " << #x << " is " << x << endl
+#define OUT(x) cout << x << endl
+#define OUTH(x) cout << x << " "
+#define debug(...) fprintf(stderr, __VA_ARGS__)
+#define READ(x) for(auto &(z):x) cin >> z;
+#define FOR(a, b, c) for (int(a)=(b); (a) < (c); ++(a))
+#define FORN(a, b, c) for (int(a)=(b); (a) <= (c); ++(a))
+#define FORD(a, b, c) for (int(a)=(b); (a) >= (c); --(a))
+#define FORSQ(a, b, c) for (int(a)=(b); (a) * (a) <= (c); ++(a))
+#define FORC(a, b, c) for (char(a)=(b); (a) <= (c); ++(a))
+#define EACH(a, b) for (auto&(a) : (b))
+#define REP(i, n) FOR(i, 0, n)
+#define REPN(i, n) FORN(i, 1, n)
+#define MAX(a, b) a=max(a, b)
+#define MIN(a, b) a=min(a, b)
+#define SQR(x) ((ll)(x) * (x))
+#define RESET(a, b) memset(a, b, sizeof(a))
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define ALL(v) v.begin(), v.end()
+#define ALLA(arr, sz) arr, arr + sz
+#define SIZE(v) (int)v.size()
+#define SORT(v) sort(ALL(v))
+#define REVERSE(v) reverse(ALL(v))
+#define SORTA(arr, sz) sort(ALLA(arr, sz))
+#define REVERSEA(arr, sz) reverse(ALLA(arr, sz))
+#define PERMUTE next_permutation
+#define TC(t) while (t--)
+#define FAST_INP  ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define what_is(x) cerr << #x << " is " << x << endl
+
+// even odd even odd even odd ...
+// E = O || E = O + 1
+
+void solve() {
+	int n; cin >> n;
+	int even = 0, odd = 0, ans = 0, ok;
+	REP(i, n) {
+		int x; cin >> x;
+		if(x & 1) odd++;
+		else even++;
+	}
+	if(even > odd) {
+		// even + even = even
+		// even + odd  = odd
+		// take 1 even and 1 odd cow * odd times
+		// 1 3 5 2 4 6 8 10 12
+		// (2) (1) (4) (3) (6) (5) (8 10 12)
+		OUT(2 * odd + 1);
+		return;
+	} else {
+		// odd + odd = even
+		// pair 2 odd cows
+		// 1 3 5 7 9 11 2 4 6
+		// (2) (1) (4) (3) (6) (5) (9 11) (7) 
+		ans += even * 2, odd -= even, ok = 1;
+		while(odd > 0) {
+			if(ok) ans++, odd -= 2, ok = 0;
+			else ans++, odd--, ok = 1;
+		}
+		if(odd < 0) ans -= 2;
+	}
+	OUT(ans);
+}
+
+
+int main()
+{
+    FAST_INP;
+//    #ifndef ONLINE_JUDGE
+//    freopen("input.txt","r", stdin);
+//    freopen("output.txt","w", stdout);
+//    #endif
+
+    // int tc; cin >> tc;
+    // TC(tc) solve();
+    solve();
+    return 0;
+}
