5498ef4f7b9bce7a1f3693c75ba65dc290ab0459
wingkwong <wingkwong.code@gmail.com>
2021-01-31 13:41:29 +0800

    Update 695-max-area-of-island.cpp

--- leetcode/problems/medium/695-max-area-of-island.cpp
+++ leetcode/problems/medium/695-max-area-of-island.cpp
@@ -23,7 +23,33 @@ Given the above grid, return 0.
 Note: The length of each dimension in the given grid does not exceed 50.
 */
 
+
 class Solution {
+public:
+    int ans = 0;
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        for(int i = 0; i < grid.size(); i++) {
+            for(int j = 0; j < grid[0].size(); j++) {
+                if(grid[i][j] == 1) {
+                    ans = max(ans, dfs(grid, i, j));
+                }
+            }
+        }
+        return ans;
+    }
+    
+    int dfs(vector<vector<int>>& grid, int i, int j) {
+        if(i < 0 || j < 0 || i > grid.size() - 1 || j > grid[0].size() - 1 || grid[i][j] != 1) return 0;
+        grid[i][j] = 0;
+        return 1 +
+            dfs(grid, i + 1, j) +
+            dfs(grid, i, j + 1) +
+            dfs(grid, i - 1, j) +
+            dfs(grid, i, j - 1);
+    }
+};
+
+class Solution2 {
 public:
     int maxAreaOfIsland(vector<vector<int>>& grid) {
         int r=(int)grid.size();
