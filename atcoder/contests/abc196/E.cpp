069676a23952a67da856637defc732a30e01748e
wingkwong <wingkwong.code@gmail.com>
2021-03-21 18:59:19 +0800

    AtCoder Beginner Contest 196

---
+++ atcoder/contests/abc196/E.cpp
@@ -0,0 +1,92 @@
+/** 
+* Time: 2021-03-18 22:50:25
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<string, string> pss;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+
+double EPS=1e-9;
+int INF=1000000005;
+long long INFF=1000000000000000005ll;
+double PI=acos(-1);
+int dirx[8]={ -1, 0, 0, 1, -1, -1, 1, 1 };
+int diry[8]={ 0, 1, -1, 0, -1, 1, -1, 1 };
+ll MOD = 1000000007;
+
+#define DEBUG fprintf(stderr, "====TESTING====\n")
+#define VALUE(x) cerr << "The value of " << #x << " is " << x << endl
+#define OUT(x) cout << x << endl
+#define OUTH(x) cout << x << " "
+#define debug(...) fprintf(stderr, __VA_ARGS__)
+#define READ(x) for(auto &(z):x) cin >> z;
+#define FOR(a, b, c) for (int(a)=(b); (a) < (c); ++(a))
+#define FORN(a, b, c) for (int(a)=(b); (a) <= (c); ++(a))
+#define FORD(a, b, c) for (int(a)=(b); (a) >= (c); --(a))
+#define FORSQ(a, b, c) for (int(a)=(b); (a) * (a) <= (c); ++(a))
+#define FORC(a, b, c) for (char(a)=(b); (a) <= (c); ++(a))
+#define EACH(a, b) for (auto&(a) : (b))
+#define REP(i, n) FOR(i, 0, n)
+#define REPN(i, n) FORN(i, 1, n)
+#define MAX(a, b) a=max(a, b)
+#define MIN(a, b) a=min(a, b)
+#define SQR(x) ((ll)(x) * (x))
+#define RESET(a, b) memset(a, b, sizeof(a))
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define ALL(v) v.begin(), v.end()
+#define ALLA(arr, sz) arr, arr + sz
+#define SIZE(v) (int)v.size()
+#define SORT(v) sort(ALL(v))
+#define REVERSE(v) reverse(ALL(v))
+#define SORTA(arr, sz) sort(ALLA(arr, sz))
+#define REVERSEA(arr, sz) reverse(ALLA(arr, sz))
+#define PERMUTE next_permutation
+#define TC(t) while (t--)
+#define FAST_INP  ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define what_is(x) cerr << #x << " is " << x << endl
+
+void solve() {
+	ll N; cin >> N;
+	vl A(N), T(N);
+	REP(i, N) cin >> A[i] >> T[i];
+	ll Q; cin >> Q;
+	vl X(Q);
+	READ(X);
+	// F(x) = min(c, max(b, x + a))
+	// t[i] = 1 : F(x) = min(inf, max(-inf, x + a[i]))
+	// t[i] = 2 : F(x) = min(inf, max(a[i], x))
+	// t[i] = 3 : F(x) = min(a[i], max(-inf, x))
+	ll mi = -INFF, mx = INFF, x = 0;
+	REP(i, N) {
+		if(T[i] == 1) x += A[i];
+		else if(T[i] == 2) mi = max<ll>(mi, A[i] - x), mx = max<ll>(mx, mi);
+		else mx = min<ll>(mx, A[i] - x), mi = min<ll>(mx, mi);
+	}
+	REP(i, Q) OUT(max<ll>(mi, min<ll>(mx, X[i])) + x);
+}
+ 
+int main()
+{
+    FAST_INP;
+//    #ifndef ONLINE_JUDGE
+//    freopen("input.txt","r", stdin);
+//    freopen("output.txt","w", stdout);
+//    #endif
+ 
+    // int tc; cin >> tc;
+    // TC(tc) solve();
+    solve();
+    return 0;
+}
