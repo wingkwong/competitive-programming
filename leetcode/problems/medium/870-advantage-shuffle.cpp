241e3f0b0814bf7588cf92b24004fd92416da841
wingkwong <wingkwong.code@gmail.com>
2021-03-24 21:04:28 +0800

    Create 870-advantage-shuffle.cpp

---
+++ leetcode/problems/medium/870-advantage-shuffle.cpp
@@ -0,0 +1,40 @@
+/*
+Advantage Shuffle
+https://leetcode.com/problems/advantage-shuffle/
+
+Given two arrays A and B of equal size, the advantage of A with respect to B is the number of indices i for which A[i] > B[i].
+
+Return any permutation of A that maximizes its advantage with respect to B.
+
+ 
+
+Example 1:
+
+Input: A = [2,7,11,15], B = [1,10,4,11]
+Output: [2,11,7,15]
+Example 2:
+
+Input: A = [12,24,8,32], B = [13,25,32,11]
+Output: [24,32,8,12]
+ 
+
+Note:
+
+1 <= A.length = B.length <= 10000
+0 <= A[i] <= 10^9
+0 <= B[i] <= 10^9
+*/
+
+class Solution {
+public:
+    vector<int> advantageCount(vector<int>& A, vector<int>& B) {
+        multiset<int> s(A.begin(), A.end());
+        // find the least bigger element or take the smallest one
+        for(int i = 0; i < B.size(); i++) {
+            auto it = *s.rbegin() > B[i] ? s.upper_bound(B[i]) : s.begin();
+            A[i] = *it;
+            s.erase(it);
+        }
+        return A;
+    }
+};
