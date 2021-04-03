09ae9deffff37947da9f41a6c88b80995aea28f5
wingkwong <wingkwong.code@gmail.com>
2021-04-03 18:57:07 +0800

    Create minimalism.cpp

---
+++ template/C++/minimalism.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+void solve() {
+
+}
+ 
+int main() {
+	using namespace std;
+	ios_base::sync_with_stdio(false), cin.tie(nullptr);
+ 
+ 	int T; cin >> T;
+ 	while(T--) solve();
+ 	// solve();
+	return 0;
+}
