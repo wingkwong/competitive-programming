cb36214103fc487be7e48e7bb0b134699108c96b
wingkwong <wingkwong.code@gmail.com>
2021-04-06 01:04:38 +0800

    Create 775-global-and-local-inversions.cpp

---
+++ leetcode/problems/medium/775-global-and-local-inversions.cpp
@@ -0,0 +1,39 @@
+/*
+Global and Local Inversions
+https://leetcode.com/problems/global-and-local-inversions/
+
+We have some permutation A of [0, 1, ..., N - 1], where N is the length of A.
+
+The number of (global) inversions is the number of i < j with 0 <= i < j < N and A[i] > A[j].
+
+The number of local inversions is the number of i with 0 <= i < N and A[i] > A[i+1].
+
+Return true if and only if the number of global inversions is equal to the number of local inversions.
+
+Example 1:
+
+Input: A = [1,0,2]
+Output: true
+Explanation: There is 1 global inversion, and 1 local inversion.
+Example 2:
+
+Input: A = [1,2,0]
+Output: false
+Explanation: There are 2 global inversions, and 1 local inversion.
+Note:
+
+A will be a permutation of [0, 1, ..., A.length - 1].
+A will have length in range [1, 5000].
+The time limit for this problem has been reduced.
+*/
+
+class Solution {
+public:
+    bool isIdealPermutation(vector<int>& A) {
+        for (int i = 0; i < A.size(); ++i) {
+        	// A[i] - i must either -1, 0 or 1
+        	if (abs(A[i] - i) > 1) return false;
+        }
+        return true;
+    }
+};
