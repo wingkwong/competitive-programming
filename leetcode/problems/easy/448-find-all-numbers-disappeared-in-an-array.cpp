da61e558a46d36c5853f21ca6e709f2ad633d80e
wingkwong <wingkwong.code@gmail.com>
2021-04-24 00:21:21 +0800

    Update 448-find-all-numbers-disappeared-in-an-array.cpp

--- leetcode/problems/easy/448-find-all-numbers-disappeared-in-an-array.cpp
+++ leetcode/problems/easy/448-find-all-numbers-disappeared-in-an-array.cpp
@@ -46,4 +46,22 @@ public:
     }
 };
 
+class Solution3 {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        int n = nums.size();
+        for(int i = 0; i < n; i++) {
+            int m = abs(nums[i]) - 1;
+            nums[m] = nums[m] > 0 ? -nums[m] : nums[m];
+        }
+        vector<int> ans;
+        for(int i = 0; i < n; i++) {
+            if(nums[i] > 0) {
+                ans.push_back(i + 1);
+            }
+        }
+        return ans;
+    }
+};
+
 static const auto io_sync_off = []() {std::ios::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);return 0;}();
