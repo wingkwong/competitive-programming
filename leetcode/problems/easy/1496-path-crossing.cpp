a30c17025884b1ae6b2debf1602e6e8111d99d7a
wingkwong <wingkwong.code@gmail.com>
2021-04-23 22:11:30 +0800

    Create 1496-path-crossing.cpp

---
+++ leetcode/problems/easy/1496-path-crossing.cpp
@@ -0,0 +1,44 @@
+/*
+Path Crossing
+https://leetcode.com/problems/path-crossing/
+
+
+Given a string path, where path[i] = 'N', 'S', 'E' or 'W', each representing moving one unit north, south, east, or west, respectively. You start at the origin (0, 0) on a 2D plane and walk on the path specified by path.
+
+Return True if the path crosses itself at any point, that is, if at any time you are on a location you've previously visited. Return False otherwise.
+
+Example 1:
+
+Input: path = "NES"
+Output: false 
+Explanation: Notice that the path doesn't cross any point more than once.
+
+Example 2:
+
+Input: path = "NESWW"
+Output: true
+Explanation: Notice that the path visits the origin twice.
+
+Constraints:
+
+1 <= path.length <= 10^4
+path will only consist of characters in {'N', 'S', 'E', 'W}
+*/
+
+class Solution {
+public:
+    bool isPathCrossing(string path) {
+        int x = 0, y = 0;
+        set<pair<int, int>> s;
+        s.insert({x, y});
+        for(char c : path) {
+            if(c == 'N') y++;
+            else if(c == 'S') y--;
+            else if(c == 'E') x++;
+            else x--;
+            if(s.find({x, y}) != s.end()) return true;
+            s.insert({x, y});
+        }
+        return false;
+    }
+};
