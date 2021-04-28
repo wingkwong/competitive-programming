cb416b137464dfeec8bc0c3917b44d8cacc27ede
wingkwong <wingkwong.code@gmail.com>
2021-04-28 21:28:07 +0800

    Update 63-unique-paths-ii.cpp

--- leetcode/problems/medium/63-unique-paths-ii.cpp
+++ leetcode/problems/medium/63-unique-paths-ii.cpp
@@ -30,6 +30,26 @@ There are two ways to reach the bottom-right corner:
 class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int ans = 0, n = obstacleGrid.size(), m = obstacleGrid[0].size();
+        vector<vector<int>> dp(n, vector<int>(m, 0));
+        dp[0][0] = 1;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(obstacleGrid[i][j] == 0) {
+                    if(i) dp[i][j] += dp[i - 1][j];
+                    if(j) dp[i][j] += dp[i][j - 1];
+                } else {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        return dp[n - 1][m - 1];
+    }
+};
+
+class Solution2 {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         // similiar to 62 - Unique Paths 
         int m=obstacleGrid.size(), n=obstacleGrid[0].size();
         int dp[m][n];
