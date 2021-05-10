f9bf3934e204b08e047fd0c6cc4cd11991d6a88c
wingkwong <wingkwong.code@gmail.com>
2021-05-10 19:39:00 +0800

    Update 204-count-primes.cpp

--- leetcode/problems/easy/204-count-primes.cpp
+++ leetcode/problems/easy/204-count-primes.cpp
@@ -12,6 +12,30 @@ Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
 
 class Solution {
 public:
+    vector<bool> seiveOfEratosthenes(const int n) {
+      vector<bool> isPrime(n + 1, true);
+      isPrime[0] = isPrime[1] = 0;
+      for (int i = 2; i * i <= n; i++) {
+        if (isPrime[i]) {
+          for (int j = i * i; j <= n; j += i) {
+            isPrime[j] = false;
+          }
+        }
+      }
+      return isPrime;
+    }
+
+    int countPrimes(int n) {
+        if(n < 2) return 0;
+        vector<bool> isPrime = seiveOfEratosthenes(n - 1);
+        int cnt = 0;
+        for(int i = 2; i < n; i++) cnt += isPrime[i] == true;
+        return cnt;
+    }
+};
+
+class Solution2 {
+public:
     int countPrimes(int n) {
 //     algorithm Sieve of Eratosthenes is
 //     input: an integer n > 1.
