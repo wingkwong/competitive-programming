3f532c7cb5d40074b16b10332c956ce0647a5f3d
wingkwong <wingkwong.code@gmail.com>
2021-05-24 20:58:52 +0800

    Unsolve F - Unusual Matrix

---
+++ codeforces/contests/1475/F.cpp
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<string, string> pss;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+
+double EPS=1e-9;
+int INF=1000000005;
+long long INFF=1000000000000000005ll;
+double PI=acos(-1);
+int dirx[8]={ -1, 0, 0, 1, -1, -1, 1, 1 };
+int diry[8]={ 0, 1, -1, 0, -1, 1, -1, 1 };
+ll MOD = 1000000007;
+
+#define DEBUG fprintf(stderr, "====TESTING====\n")
+#define VALUE(x) cerr << "The value of " << #x << " is " << x << '\n'
+#define OUT(x) cout << x << '\n'
+#define OUTH(x) cout << x << " "
+#define debug(...) fprintf(stderr, __VA_ARGS__)
+#define READ(x) for(auto &(z):x) cin >> z;
+#define FOR(a, b, c) for (int(a)=(b); (a) < (c); ++(a))
+#define FORN(a, b, c) for (int(a)=(b); (a) <= (c); ++(a))
+#define FORD(a, b, c) for (int(a)=(b); (a) >= (c); --(a))
+#define FORSQ(a, b, c) for (int(a)=(b); (a) * (a) <= (c); ++(a))
+#define FORC(a, b, c) for (char(a)=(b); (a) <= (c); ++(a))
+#define EACH(a, b) for (auto&(a) : (b))
+#define REP(i, n) FOR(i, 0, n)
+#define REPN(i, n) FORN(i, 1, n)
+#define MAX(a, b) a=max(a, b)
+#define MIN(a, b) a=min(a, b)
+#define SQR(x) ((ll)(x) * (x))
+#define RESET(a, b) memset(a, b, sizeof(a))
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define ALL(v) v.begin(), v.end()
+#define ALLA(arr, sz) arr, arr + sz
+#define SIZE(v) (int)v.size()
+#define SORT(v) sort(ALL(v))
+#define REVERSE(v) reverse(ALL(v))
+#define SORTA(arr, sz) sort(ALLA(arr, sz))
+#define REVERSEA(arr, sz) reverse(ALLA(arr, sz))
+#define PERMUTE next_permutation
+#define TC(t) while (t--)
+#define FAST_INP  ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define what_is(x) cerr << #x << " is " << x << '\n'
+
+template<typename T_vector>
+void output_vector(const T_vector &v, bool line_break = false, bool add_one = false, int start = -1, int end = -1) {
+    if (start < 0) start = 0;
+    if (end < 0) end = int(v.size());
+    for (int i = start; i < end; i++) {
+      cout << v[i] + (add_one ? 1 : 0) << (line_break ? '\n' : i < end - 1 ? ' ' : '\n');
+    }
+}
+
+void solve() {
+    int n; cin >> n;
+    vvi a(n, vi(n)), b(n, vi(n));
+    char c;
+    REP(i, n) REP(j, n) {
+        cin >> c;
+        a[i][j] = c - 48;
+    }
+    REP(i, n) REP(j, n) {
+        cin >> c;
+        b[i][j] = c - 48;
+    }
+    REP(i, n) {
+        // fix first row
+        if(a[0][i] != b[0][i]) {
+            REP(j, n) a[j][i] ^= 1;
+        }
+    }
+    REP(i, n) {
+        // fix first column
+        if(a[i][0] != b[i][0]) {
+            REP(j, n) a[i][j] ^= 1;
+        }
+    }
+    if(a == b) OUT("YES");
+    else OUT("NO");
+}
+ 
+int main() {
+    FAST_INP;
+
+//    #ifndef ONLINE_JUDGE
+//    freopen("input.txt","r", stdin);
+//    freopen("output.txt","w", stdout);
+//    #endif
+ 
+    int tc; cin >> tc;
+    TC(tc) solve();
+    // solve();
+    return 0;
+}
