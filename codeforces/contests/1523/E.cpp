01c82dab40acb18c7fad5c1ecf98c15ccd464b68
Wing-Kam Wong <wingkwong.code@gmail.com>
2021-06-12 18:03:04 +0800

    Upsolve Crypto Lights

---
+++ codeforces/contests/1523/E.cpp
@@ -0,0 +1,296 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<string, string> pss;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+
+double EPS=1e-9;
+int INF=1000000005;
+long long INFF=1000000000000000005ll;
+double PI=acos(-1);
+int dirx[8]={ -1, 0, 0, 1, -1, -1, 1, 1 };
+int diry[8]={ 0, 1, -1, 0, -1, 1, -1, 1 };
+// ll MOD = 1000000007;
+
+#define DEBUG fprintf(stderr, "====TESTING====\n")
+#define VALUE(x) cerr << "The value of " << #x << " is " << x << '\n'
+#define OUT(x) cout << x << '\n'
+#define OUTH(x) cout << x << " "
+#define debug(...) fprintf(stderr, __VA_ARGS__)
+#define READ(x) for(auto &(z):x) cin >> z;
+#define FOR(a, b, c) for (int(a)=(b); (a) < (c); ++(a))
+#define FORN(a, b, c) for (int(a)=(b); (a) <= (c); ++(a))
+#define FORD(a, b, c) for (int(a)=(b); (a) >= (c); --(a))
+#define FORSQ(a, b, c) for (int(a)=(b); (a) * (a) <= (c); ++(a))
+#define FORC(a, b, c) for (char(a)=(b); (a) <= (c); ++(a))
+#define EACH(a, b) for (auto&(a) : (b))
+#define REP(i, n) FOR(i, 0, n)
+#define REPN(i, n) FORN(i, 1, n)
+#define MAX(a, b) a=max(a, b)
+#define MIN(a, b) a=min(a, b)
+#define SQR(x) ((ll)(x) * (x))
+#define RESET(a, b) memset(a, b, sizeof(a))
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define ALL(v) v.begin(), v.end()
+#define ALLA(arr, sz) arr, arr + sz
+#define SIZE(v) (int)v.size()
+#define SORT(v) sort(ALL(v))
+#define REVERSE(v) reverse(ALL(v))
+#define SORTA(arr, sz) sort(ALLA(arr, sz))
+#define REVERSEA(arr, sz) reverse(ALLA(arr, sz))
+#define PERMUTE next_permutation
+#define TC(t) while (t--)
+#define FAST_INP  ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define what_is(x) cerr << #x << " is " << x << '\n'
+
+template<const int &MOD>
+struct _m_int {
+    int val;
+ 
+    _m_int(int64_t v = 0) {
+        if (v < 0) v = v % MOD + MOD;
+        if (v >= MOD) v %= MOD;
+        val = int(v);
+    }
+ 
+    _m_int(uint64_t v) {
+        if (v >= MOD) v %= MOD;
+        val = int(v);
+    }
+ 
+    _m_int(int v) : _m_int(int64_t(v)) {}
+    _m_int(unsigned v) : _m_int(uint64_t(v)) {}
+ 
+    explicit operator int() const { return val; }
+    explicit operator unsigned() const { return val; }
+    explicit operator int64_t() const { return val; }
+    explicit operator uint64_t() const { return val; }
+    explicit operator double() const { return val; }
+    explicit operator long double() const { return val; }
+ 
+    _m_int& operator+=(const _m_int &other) {
+        val -= MOD - other.val;
+        if (val < 0) val += MOD;
+        return *this;
+    }
+ 
+    _m_int& operator-=(const _m_int &other) {
+        val -= other.val;
+        if (val < 0) val += MOD;
+        return *this;
+    }
+ 
+    static unsigned fast_mod(uint64_t x, unsigned m = MOD) {
+#if !defined(_WIN32) || defined(_WIN64)
+        return unsigned(x % m);
+#endif
+        // Optimized mod for Codeforces 32-bit machines.
+        // x must be less than 2^32 * m for this to work, so that x / m fits in an unsigned 32-bit int.
+        unsigned x_high = unsigned(x >> 32), x_low = unsigned(x);
+        unsigned quot, rem;
+        asm("divl %4\n"
+            : "=a" (quot), "=d" (rem)
+            : "d" (x_high), "a" (x_low), "r" (m));
+        return rem;
+    }
+ 
+    _m_int& operator*=(const _m_int &other) {
+        val = fast_mod(uint64_t(val) * other.val);
+        return *this;
+    }
+ 
+    _m_int& operator/=(const _m_int &other) {
+        return *this *= other.inv();
+    }
+ 
+    friend _m_int operator+(const _m_int &a, const _m_int &b) { return _m_int(a) += b; }
+    friend _m_int operator-(const _m_int &a, const _m_int &b) { return _m_int(a) -= b; }
+    friend _m_int operator*(const _m_int &a, const _m_int &b) { return _m_int(a) *= b; }
+    friend _m_int operator/(const _m_int &a, const _m_int &b) { return _m_int(a) /= b; }
+ 
+    _m_int& operator++() {
+        val = val == MOD - 1 ? 0 : val + 1;
+        return *this;
+    }
+ 
+    _m_int& operator--() {
+        val = val == 0 ? MOD - 1 : val - 1;
+        return *this;
+    }
+ 
+    _m_int operator++(int) { _m_int before = *this; ++*this; return before; }
+    _m_int operator--(int) { _m_int before = *this; --*this; return before; }
+ 
+    _m_int operator-() const {
+        return val == 0 ? 0 : MOD - val;
+    }
+ 
+    friend bool operator==(const _m_int &a, const _m_int &b) { return a.val == b.val; }
+    friend bool operator!=(const _m_int &a, const _m_int &b) { return a.val != b.val; }
+    friend bool operator<(const _m_int &a, const _m_int &b) { return a.val < b.val; }
+    friend bool operator>(const _m_int &a, const _m_int &b) { return a.val > b.val; }
+    friend bool operator<=(const _m_int &a, const _m_int &b) { return a.val <= b.val; }
+    friend bool operator>=(const _m_int &a, const _m_int &b) { return a.val >= b.val; }
+ 
+    static const int SAVE_INV = int(1e6) + 5;
+    static _m_int save_inv[SAVE_INV];
+ 
+    static void prepare_inv() {
+        // Make sure MOD is prime, which is necessary for the inverse algorithm below.
+        for (int64_t p = 2; p * p <= MOD; p += p % 2 + 1)
+            assert(MOD % p != 0);
+ 
+        save_inv[0] = 0;
+        save_inv[1] = 1;
+ 
+        for (int i = 2; i < SAVE_INV; i++)
+            save_inv[i] = save_inv[MOD % i] * (MOD - MOD / i);
+    }
+ 
+    _m_int inv() const {
+        if (save_inv[1] == 0)
+            prepare_inv();
+ 
+        if (val < SAVE_INV)
+            return save_inv[val];
+ 
+        _m_int product = 1;
+        int v = val;
+ 
+        while (v >= SAVE_INV) {
+            product *= MOD - MOD / v;
+            v = MOD % v;
+        }
+ 
+        return product * save_inv[v];
+    }
+ 
+    _m_int pow(int64_t p) const {
+        if (p < 0)
+            return inv().pow(-p);
+ 
+        _m_int a = *this, result = 1;
+ 
+        while (p > 0) {
+            if (p & 1)
+                result *= a;
+ 
+            p >>= 1;
+ 
+            if (p > 0)
+                a *= a;
+        }
+ 
+        return result;
+    }
+ 
+    friend ostream& operator<<(ostream &os, const _m_int &m) {
+        return os << m.val;
+    }
+};
+ 
+template<const int &MOD> _m_int<MOD> _m_int<MOD>::save_inv[_m_int<MOD>::SAVE_INV];
+ 
+extern const int MOD = int(1e9) + 7;
+using mod_int = _m_int<MOD>;
+ 
+ 
+vector<mod_int> _factorial = {1, 1}, _inv_factorial = {1, 1};
+ 
+void prepare_factorials(int64_t maximum) {
+    static int prepared_maximum = 1;
+ 
+    if (maximum <= prepared_maximum)
+        return;
+ 
+    // Prevent increasing maximum by only 1 each time.
+    maximum += maximum / 16;
+    _factorial.resize(maximum + 1);
+    _inv_factorial.resize(maximum + 1);
+ 
+    for (int i = prepared_maximum + 1; i <= maximum; i++) {
+        _factorial[i] = i * _factorial[i - 1];
+        _inv_factorial[i] = _inv_factorial[i - 1] / i;
+    }
+ 
+    prepared_maximum = int(maximum);
+}
+ 
+mod_int factorial(int n) {
+    if (n < 0) return 0;
+    prepare_factorials(n);
+    return _factorial[n];
+}
+ 
+mod_int inv_factorial(int n) {
+    if (n < 0) return 0;
+    prepare_factorials(n);
+    return _inv_factorial[n];
+}
+ 
+mod_int choose(int64_t n, int64_t r) {
+    if (r < 0 || r > n) return 0;
+    prepare_factorials(n);
+    return _factorial[n] * _inv_factorial[r] * _inv_factorial[n - r];
+}
+ 
+mod_int permute(int64_t n, int64_t r) {
+    if (r < 0 || r > n) return 0;
+    prepare_factorials(n);
+    return _factorial[n] * _inv_factorial[n - r];
+}
+ 
+mod_int inv_choose(int64_t n, int64_t r) {
+    assert(0 <= r && r <= n);
+    prepare_factorials(n);
+    return _inv_factorial[n] * _factorial[r] * _factorial[n - r];
+}
+ 
+mod_int inv_permute(int64_t n, int64_t r) {
+    assert(0 <= r && r <= n);
+    prepare_factorials(n);
+    return _inv_factorial[n] * _factorial[n - r];
+}
+
+template<typename T_vector>
+void output_vector(const T_vector &v, bool line_break = false, bool add_one = false, int start = -1, int end = -1) {
+    if (start < 0) start = 0;
+    if (end < 0) end = int(v.size());
+    for (int i = start; i < end; i++) {
+      cout << v[i] + (add_one ? 1 : 0) << (line_break ? '\n' : i < end - 1 ? ' ' : '\n');
+    }
+}
+
+void solve() {
+	ll n, k; cin >> n >> k;
+    mod_int ans = 1;
+    FORN(m, 1, n) ans += choose(n - 1 - (m - 1) * k + m, m) / choose(n, m);
+    OUT(ans);
+}
+ 
+int main() {
+    FAST_INP;
+
+//    #ifndef ONLINE_JUDGE
+//    freopen("input.txt","r", stdin);
+//    freopen("output.txt","w", stdout);
+//    #endif
+ 
+    int tc; cin >> tc;
+    TC(tc) solve();
+    // solve();
+    return 0;
+}
