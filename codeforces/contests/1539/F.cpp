b934fc71ac785fcdba49d1710ce99afbef004a60
Wing-Kam Wong <wingkwong.code@gmail.com>
2021-06-21 23:30:49 +0800

    Upsolve F. Strange Array

---
+++ codeforces/contests/1539/F.cpp
@@ -0,0 +1,339 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<string, string> pss;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+
+double EPS=1e-9;
+int INF=1000000005;
+long long INFF=1000000000000000005ll;
+double PI=acos(-1);
+int dirx[8]={ -1, 0, 0, 1, -1, -1, 1, 1 };
+int diry[8]={ 0, 1, -1, 0, -1, 1, -1, 1 };
+
+#define DEBUG fprintf(stderr, "====TESTING====\n")
+#define VALUE(x) cerr << "The value of " << #x << " is " << x << '\n'
+#define OUT(x) cout << x << '\n'
+#define OUTH(x) cout << x << " "
+#define debug(...) fprintf(stderr, __VA_ARGS__)
+#define READ(x) for(auto &(z):x) cin >> z;
+#define FOR(a, b, c) for (int(a)=(b); (a) < (c); ++(a))
+#define FORN(a, b, c) for (int(a)=(b); (a) <= (c); ++(a))
+#define FORD(a, b, c) for (int(a)=(b); (a) >= (c); --(a))
+#define FORSQ(a, b, c) for (int(a)=(b); (a) * (a) <= (c); ++(a))
+#define FORC(a, b, c) for (char(a)=(b); (a) <= (c); ++(a))
+#define EACH(a, b) for (auto&(a) : (b))
+#define REP(i, n) FOR(i, 0, n)
+#define REPN(i, n) FORN(i, 1, n)
+#define MAX(a, b) a=max(a, b)
+#define MIN(a, b) a=min(a, b)
+#define SQR(x) ((ll)(x) * (x))
+#define RESET(a, b) memset(a, b, sizeof(a))
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define ALL(v) v.begin(), v.end()
+#define ALLA(arr, sz) arr, arr + sz
+#define SORT(v) sort(ALL(v))
+#define REVERSE(v) reverse(ALL(v))
+#define SORTA(arr, sz) sort(ALLA(arr, sz))
+#define REVERSEA(arr, sz) reverse(ALLA(arr, sz))
+#define PERMUTE next_permutation
+#define TC(t) while (t--)
+#define FAST_INP  ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define what_is(x) cerr << #x << " is " << x << '\n'
+
+// ************************DEBUG START********************************
+template<typename T> int SIZE(T (&t)){ return t.size(); } template<typename T, size_t N> int SIZE(T (&t)[N]){ return N; } string to_string(char t){ return "'" + string({t}) + "'"; } string to_string(bool t){ return t ? "true" : "false"; } string to_string(const string &t, int x1=0, int x2=1e9){ string ret = ""; for(int i = min(x1,SIZE(t)), _i = min(x2,SIZE(t)-1); i <= _i; ++i){ ret += t[i]; } return '"' + ret + '"'; } string to_string(const char* t){ string ret(t); return to_string(ret); } template<size_t N> string to_string(const bitset<N> &t, int x1=0, int x2=1e9){ string ret = ""; for(int i = min(x1,SIZE(t)); i <= min(x2,SIZE(t)-1); ++i){ ret += t[i] + '0'; } return to_string(ret); } template<typename T, typename... Coords> string to_string(const T (&t), int x1=0, int x2=1e9, Coords... C); template<typename T, typename S> string to_string(const pair<T, S> &t){ return "(" + to_string(t.first) + ", " + to_string(t.second) + ")"; } template<typename T, typename... Coords> string to_string(const T (&t), int x1, int x2, Coords... C){ string ret = "["; x1 = min(x1, SIZE(t)); auto e = begin(t); advance(e,x1); for(int i = x1, _i = min(x2,SIZE(t)-1); i <= _i; ++i){ ret += to_string(*e, C...) + (i != _i ? ", " : ""); e = next(e); } return ret + "]"; } template<int Index, typename... Ts> struct print_tuple{ string operator() (const tuple<Ts...>& t) { string ret = print_tuple<Index - 1, Ts...>{}(t); ret += (Index ? ", " : ""); return ret + to_string(get<Index>(t)); } }; template<typename... Ts> struct print_tuple<0, Ts...> { string operator() (const tuple<Ts...>& t) { return to_string(get<0>(t)); } }; template<typename... Ts> string to_string(const tuple<Ts...>& t) { const auto Size = tuple_size<tuple<Ts...>>::value; return print_tuple<Size - 1, Ts...>{}(t); } void dbgr(){;} template<typename Heads, typename... Tails> void dbgr(Heads H, Tails... T){ cout << to_string(H) << " | "; dbgr(T...); } void dbgs(){;} template<typename Heads, typename... Tails> void dbgs(Heads H, Tails... T){ cout << H << " "; dbgs(T...); } 
+#define dbgv(...) cout << to_string(__VA_ARGS__) << endl;
+#define dbg(...) cout << "[" << #__VA_ARGS__ << "]: "; dbgv(__VA_ARGS__);
+#define dbgr(...) dbgr(__VA_ARGS__); cout << endl;
+#define dbgm(...) cout << "[" << #__VA_ARGS__ << "]: "; dbgr(__VA_ARGS__);
+// ************************DEBUG END**********************************
+
+const int MOD = 1000000007;
+
+template<typename T_vector>
+void output_vector(const T_vector &v, bool line_break = false, bool add_one = false, int start = -1, int end = -1) {
+    if (start < 0) start = 0;
+    if (end < 0) end = int(v.size());
+    for (int i = start; i < end; i++) {
+      cout << v[i] + (add_one ? 1 : 0) << (line_break ? '\n' : i < end - 1 ? ' ' : '\n');
+    }
+}
+
+class segtree {
+ public:
+  struct node {
+    int mx = 0;
+    int mn = 0;
+    int add = 0;
+ 
+    void apply(int l, int r, int v) {
+      mx += v;
+      mn += v;
+      add += v;
+    }
+  };
+ 
+  node unite(const node &a, const node &b) const {
+    node res;
+    res.mn = min(a.mn, b.mn);
+    res.mx = max(a.mx, b.mx);
+    return res;
+  }
+ 
+  inline void push(int x, int l, int r) {
+    int y = (l + r) >> 1;
+    int z = x + ((y - l + 1) << 1);
+    if (tree[x].add != 0) {
+      tree[x + 1].apply(l, y, tree[x].add);
+      tree[z].apply(y + 1, r, tree[x].add);
+      tree[x].add = 0;
+    }
+  }
+ 
+  inline void pull(int x, int z) {
+    tree[x] = unite(tree[x + 1], tree[z]);
+  }
+ 
+  int n;
+  vector<node> tree;
+ 
+  void build(int x, int l, int r) {
+    if (l == r) {
+      return;
+    }
+    int y = (l + r) >> 1;
+    int z = x + ((y - l + 1) << 1);
+    build(x + 1, l, y);
+    build(z, y + 1, r);
+    pull(x, z);
+  }
+ 
+  template <typename M>
+  void build(int x, int l, int r, const vector<M> &v) {
+    if (l == r) {
+      tree[x].apply(l, r, v[l]);
+      return;
+    }
+    int y = (l + r) >> 1;
+    int z = x + ((y - l + 1) << 1);
+    build(x + 1, l, y, v);
+    build(z, y + 1, r, v);
+    pull(x, z);
+  }
+ 
+  node get(int x, int l, int r, int ll, int rr) {
+    if (ll <= l && r <= rr) {
+      return tree[x];
+    }
+    int y = (l + r) >> 1;
+    int z = x + ((y - l + 1) << 1);
+    push(x, l, r);
+    node res{};
+    if (rr <= y) {
+      res = get(x + 1, l, y, ll, rr);
+    } else {
+      if (ll > y) {
+        res = get(z, y + 1, r, ll, rr);
+      } else {
+        res = unite(get(x + 1, l, y, ll, rr), get(z, y + 1, r, ll, rr));
+      }
+    }
+    pull(x, z);
+    return res;
+  }
+ 
+  template <typename... M>
+  void modify(int x, int l, int r, int ll, int rr, const M&... v) {
+    if (ll <= l && r <= rr) {
+      tree[x].apply(l, r, v...);
+      return;
+    }
+    int y = (l + r) >> 1;
+    int z = x + ((y - l + 1) << 1);
+    push(x, l, r);
+    if (ll <= y) {
+      modify(x + 1, l, y, ll, rr, v...);
+    }
+    if (rr > y) {
+      modify(z, y + 1, r, ll, rr, v...);
+    }
+    pull(x, z);
+  }
+ 
+  int find_first_knowingly(int x, int l, int r, const function<bool(const node&)> &f) {
+    if (l == r) {
+      return l;
+    }
+    push(x, l, r);
+    int y = (l + r) >> 1;
+    int z = x + ((y - l + 1) << 1);
+    int res;
+    if (f(tree[x + 1])) {
+      res = find_first_knowingly(x + 1, l, y, f);
+    } else {
+      res = find_first_knowingly(z, y + 1, r, f);
+    }
+    pull(x, z);
+    return res;
+  }
+ 
+  int find_first(int x, int l, int r, int ll, int rr, const function<bool(const node&)> &f) {
+    if (ll <= l && r <= rr) {
+      if (!f(tree[x])) {
+        return -1;
+      }
+      return find_first_knowingly(x, l, r, f);
+    }
+    push(x, l, r);
+    int y = (l + r) >> 1;
+    int z = x + ((y - l + 1) << 1);
+    int res = -1;
+    if (ll <= y) {
+      res = find_first(x + 1, l, y, ll, rr, f);
+    }
+    if (rr > y && res == -1) {
+      res = find_first(z, y + 1, r, ll, rr, f);
+    }
+    pull(x, z);
+    return res;
+  }
+ 
+  int find_last_knowingly(int x, int l, int r, const function<bool(const node&)> &f) {
+    if (l == r) {
+      return l;
+    }
+    push(x, l, r);
+    int y = (l + r) >> 1;
+    int z = x + ((y - l + 1) << 1);
+    int res;
+    if (f(tree[z])) {
+      res = find_last_knowingly(z, y + 1, r, f);
+    } else {
+      res = find_last_knowingly(x + 1, l, y, f);
+    }
+    pull(x, z);
+    return res;
+  }
+ 
+  int find_last(int x, int l, int r, int ll, int rr, const function<bool(const node&)> &f) {
+    if (ll <= l && r <= rr) {
+      if (!f(tree[x])) {
+        return -1;
+      }
+      return find_last_knowingly(x, l, r, f);
+    }
+    push(x, l, r);
+    int y = (l + r) >> 1;
+    int z = x + ((y - l + 1) << 1);
+    int res = -1;
+    if (rr > y) {
+      res = find_last(z, y + 1, r, ll, rr, f);
+    }
+    if (ll <= y && res == -1) {
+      res = find_last(x + 1, l, y, ll, rr, f);
+    }
+    pull(x, z);
+    return res;
+  }
+ 
+  segtree(int _n) : n(_n) {
+    assert(n > 0);
+    tree.resize(2 * n - 1);
+    build(0, 0, n - 1);
+  }
+ 
+  template <typename M>
+  segtree(const vector<M> &v) {
+    n = v.size();
+    assert(n > 0);
+    tree.resize(2 * n - 1);
+    build(0, 0, n - 1, v);
+  }
+ 
+  node get(int ll, int rr) {
+    assert(0 <= ll && ll <= rr && rr <= n - 1);
+    return get(0, 0, n - 1, ll, rr);
+  }
+ 
+  node get(int p) {
+    assert(0 <= p && p <= n - 1);
+    return get(0, 0, n - 1, p, p);
+  }
+ 
+  template <typename... M>
+  void modify(int ll, int rr, const M&... v) {
+    assert(0 <= ll && ll <= rr && rr <= n - 1);
+    modify(0, 0, n - 1, ll, rr, v...);
+  }
+ 
+  // find_first and find_last call all FALSE elements
+  // to the left (right) of the sought position exactly once
+ 
+  int find_first(int ll, int rr, const function<bool(const node&)> &f) {
+    assert(0 <= ll && ll <= rr && rr <= n - 1);
+    return find_first(0, 0, n - 1, ll, rr, f);
+  }
+ 
+  int find_last(int ll, int rr, const function<bool(const node&)> &f) {
+    assert(0 <= ll && ll <= rr && rr <= n - 1);
+    return find_last(0, 0, n - 1, ll, rr, f);
+  }
+};
+
+void solve() {
+    int n; cin >> n;
+    vi a(n), order(n), ans(n);
+    READ(a);
+    iota(ALL(order), 0);
+    sort(ALL(order), [&](int i, int j) {
+        return a[i] < a[j];
+    });
+    segtree st(n + 1);
+    REP(i, n) st.modify(i + 1, n, 1);
+    int start = 0;
+    while(start < n) {
+        int end = start;
+        while(end + 1 < n && a[order[end + 1]] == a[order[end]]) end++;
+        FORN(j, start, end) {
+            int i = order[j];
+            auto L = st.get(0, i), R = st.get(i + 1, n);
+            MAX(ans[i], ((R.mx - 1) - L.mn + 1) / 2);
+        }
+        FORN(j, start, end) st.modify(order[j] + 1, n, -2);
+        FORN(j, start, end) {
+            int i = order[j];
+            auto L = st.get(0, i), R = st.get(i + 1, n);
+            MAX(ans[i], (L.mx - (R.mn + 1)) / 2); 
+        }
+        start = end + 1;
+    }
+    output_vector(ans);
+}
+ 
+int main() {
+    FAST_INP;
+
+//    #ifndef ONLINE_JUDGE
+//    freopen("input.txt","r", stdin);
+//    freopen("output.txt","w", stdout);
+//    #endif
+ 
+    // int tc; cin >> tc;
+    // TC(tc) solve();
+    solve();
+    return 0;
+}
