05173ea2d83c043f6dc833bfc85e3f1d10155d81
Wing-Kam Wong <wingkwong.code@gmail.com>
2021-06-30 21:13:59 +0800

    Update 236-lowest-common-ancestor-of-a-binary-tree.cpp

--- leetcode/problems/medium/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ leetcode/problems/medium/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -44,7 +44,27 @@ p and q will exist in the tree.
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (!root || root == p || root == q) return root;
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+        return !left ? right : !right ? left : root;
+    }
+};
+
+class Solution2 {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         if (!root || root == p || root == q) return root;
